417a8469813bd80f813833c1a44f6b6f4ef8c53c
Bogdan Kostevich <bogdan.kostevich@flexit.es>
2022-05-15 20:32:18 +0300

    Completed ExcelInterpreter

--- src/test/java/com/knubisoft/e2e/testing/framework/interpreter/CsvInterpreter.java
+++ src/test/java/com/knubisoft/e2e/testing/framework/interpreter/CsvInterpreter.java
@@ -1,9 +1,10 @@
 package com.knubisoft.e2e.testing.framework.interpreter;
 
 import com.knubisoft.e2e.testing.framework.configuration.TestResourceSettings;
+import com.knubisoft.e2e.testing.framework.constant.DelimiterConstant;
+import com.knubisoft.e2e.testing.framework.db.source.ListSource;
 import com.knubisoft.e2e.testing.framework.db.source.Source;
 import com.knubisoft.e2e.testing.framework.db.sql.PostgresSqlOperation;
-import com.knubisoft.e2e.testing.framework.interpreter.formater.CsvFormatter;
 import com.knubisoft.e2e.testing.framework.interpreter.lib.AbstractInterpreter;
 import com.knubisoft.e2e.testing.framework.interpreter.lib.InterpreterDependencies;
 import com.knubisoft.e2e.testing.framework.interpreter.lib.InterpreterForClass;
@@ -14,11 +15,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @InterpreterForClass(CsvCommands.class)
 public class CsvInterpreter extends AbstractInterpreter<CsvCommands> {
+
+    private static final String SQL_INSERT = "INSERT INTO %s %s";
+    private static final String SQL_VALUES = "VALUES (%s);";
 
     @Autowired(required = false)
     private PostgresSqlOperation postgresSqlOperation;
@@ -31,15 +38,51 @@ public class CsvInterpreter extends AbstractInterpreter<CsvCommands> {
     protected void acceptImpl(final CsvCommands csvCommands, final CommandResult result) {
         csvCommands.getCsvFile().forEach(csvFile -> {
             File csv = getCsvFileByPath(csvFile);
-            List<String> commands = CsvFormatter.convertFileToString(csv);
-            Source source = CsvFormatter.getSource(commands);
+            List<String> commands = convertFileToString(csv);
+            Source source = getSource(commands);
             postgresSqlOperation.apply(source, csvCommands.getAlias());
         });
     }
 
-    public File getCsvFileByPath(final String pathToFile) {
+    private File getCsvFileByPath(final String pathToFile) {
         FileSearcher fileSearcher = dependencies.getFileSearcher();
         File csvFolder = TestResourceSettings.getInstance().getCsvFolder();
         return fileSearcher.search(csvFolder, pathToFile);
+    }
+
+    private List<String> convertFileToString(final File csvFile) {
+        try {
+            return Files.readAllLines(csvFile.toPath());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Source getSource(final List<String> commands) {
+        List<String> queries = commands.stream().map(command -> {
+            String tableName = getTableName(command);
+            String values = getValues(command);
+            if (tableName.isEmpty() || values.isEmpty()) {
+                throw new IllegalArgumentException("The syntax of query isn't correct.");
+            }
+            return String.format(SQL_INSERT, tableName, values);
+        }).collect(Collectors.toList());
+        return new ListSource(queries);
+    }
+
+    private String getTableName(final String command) {
+        return command.split(DelimiterConstant.HASH)[0] != null
+                ? command.split(DelimiterConstant.HASH)[0]
+                : DelimiterConstant.EMPTY;
+    }
+
+    private String getValues(final String values) {
+        String line = values.split(DelimiterConstant.HASH)[1] != null
+                ? values.split(DelimiterConstant.HASH)[1]
+                : DelimiterConstant.EMPTY;
+        if (line.isEmpty()) {
+            return DelimiterConstant.EMPTY;
+        }
+        return String.format(SQL_VALUES, String.join(DelimiterConstant.EMPTY, line));
     }
 }
--- src/test/java/com/knubisoft/e2e/testing/framework/interpreter/ExcelInterpreter.java
+++ src/test/java/com/knubisoft/e2e/testing/framework/interpreter/ExcelInterpreter.java
@@ -1,9 +1,10 @@
 package com.knubisoft.e2e.testing.framework.interpreter;
 
 import com.knubisoft.e2e.testing.framework.configuration.TestResourceSettings;
+import com.knubisoft.e2e.testing.framework.constant.DelimiterConstant;
+import com.knubisoft.e2e.testing.framework.db.source.ListSource;
 import com.knubisoft.e2e.testing.framework.db.source.Source;
 import com.knubisoft.e2e.testing.framework.db.sql.PostgresSqlOperation;
-import com.knubisoft.e2e.testing.framework.interpreter.formater.CsvFormatter;
 import com.knubisoft.e2e.testing.framework.interpreter.lib.AbstractInterpreter;
 import com.knubisoft.e2e.testing.framework.interpreter.lib.InterpreterDependencies;
 import com.knubisoft.e2e.testing.framework.interpreter.lib.InterpreterForClass;
@@ -14,11 +15,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @InterpreterForClass(ExcelCommands.class)
 public class ExcelInterpreter extends AbstractInterpreter<ExcelCommands> {
+
+    private static final String SQL_INSERT = "INSERT INTO %s %s";
+    private static final String SQL_VALUES = "VALUES (%s);";
 
     @Autowired(required = false)
     private PostgresSqlOperation postgresSqlOperation;
@@ -31,15 +38,51 @@ public class ExcelInterpreter extends AbstractInterpreter<ExcelCommands> {
     protected void acceptImpl(final ExcelCommands excelCommands, final CommandResult result) {
         excelCommands.getExcelFile().forEach(excelFile -> {
             File excel = getCsvFileByPath(excelFile);
-            List<String> commands = CsvFormatter.convertFileToString(excel);
-            Source source = CsvFormatter.getSource(commands);
+            List<String> commands = convertFileToString(excel);
+            Source source = getSource(commands);
             postgresSqlOperation.apply(source, excelCommands.getAlias());
         });
     }
 
-    public File getCsvFileByPath(final String pathToFile) {
+    private File getCsvFileByPath(final String pathToFile) {
         FileSearcher fileSearcher = dependencies.getFileSearcher();
         File excelFolder = TestResourceSettings.getInstance().getExcelFolder();
         return fileSearcher.search(excelFolder, pathToFile);
+    }
+
+    private List<String> convertFileToString(final File excelFile) {
+        try {
+            return Files.readAllLines(excelFile.toPath());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Source getSource(final List<String> commands) {
+        List<String> queries = commands.stream().map(command -> {
+            String tableName = getTableName(command);
+            String values = getValues(command);
+            if (tableName.isEmpty() || values.isEmpty()) {
+                throw new IllegalArgumentException("The syntax of query isn't correct.");
+            }
+            return String.format(SQL_INSERT, tableName, values);
+        }).collect(Collectors.toList());
+        return new ListSource(queries);
+    }
+
+    private String getTableName(final String command) {
+        String tableName = command.substring(0, command.indexOf(DelimiterConstant.COMA));
+        if (tableName.isEmpty()) {
+            return DelimiterConstant.EMPTY;
+        }
+        return tableName;
+    }
+
+    private String getValues(final String values) {
+        String line = values.substring(values.indexOf(DelimiterConstant.COMA) + 1);
+        if (line.isEmpty()) {
+            return DelimiterConstant.EMPTY;
+        }
+        return String.format(SQL_VALUES, String.join(DelimiterConstant.EMPTY, line));
     }
 }

4848b2cbcecf0647e42aceb112bd11a5e4961a8c
Bogdan Kostevich <bogdan.kostevich@flexit.es>
2022-05-21 23:55:46 +0300

    Fixed comments regarding ExcelInterpreter

--- src/test/java/com/knubisoft/e2e/testing/framework/interpreter/ExcelInterpreter.java
+++ src/test/java/com/knubisoft/e2e/testing/framework/interpreter/ExcelInterpreter.java
@@ -3,7 +3,6 @@ package com.knubisoft.e2e.testing.framework.interpreter;
 import com.knubisoft.e2e.testing.framework.configuration.TestResourceSettings;
 import com.knubisoft.e2e.testing.framework.constant.DelimiterConstant;
 import com.knubisoft.e2e.testing.framework.db.source.ListSource;
-import com.knubisoft.e2e.testing.framework.db.source.Source;
 import com.knubisoft.e2e.testing.framework.db.sql.PostgresSqlOperation;
 import com.knubisoft.e2e.testing.framework.interpreter.lib.AbstractInterpreter;
 import com.knubisoft.e2e.testing.framework.interpreter.lib.InterpreterDependencies;
@@ -12,13 +11,19 @@ import com.knubisoft.e2e.testing.framework.report.CommandResult;
 import com.knubisoft.e2e.testing.framework.util.FileSearcher;
 import com.knubisoft.e2e.testing.model.scenario.ExcelCommands;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @InterpreterForClass(ExcelCommands.class)
@@ -26,6 +31,9 @@ public class ExcelInterpreter extends AbstractInterpreter<ExcelCommands> {
 
     private static final String SQL_INSERT = "INSERT INTO %s %s";
     private static final String SQL_VALUES = "VALUES (%s);";
+    private static final String XLSX_EXTENSION = ".xlsx";
+    private static final String EMPTY_CELL = "'";
+    private static final String NULL_CELL = "NULL";
 
     @Autowired(required = false)
     private PostgresSqlOperation postgresSqlOperation;
@@ -37,11 +45,41 @@ public class ExcelInterpreter extends AbstractInterpreter<ExcelCommands> {
     @Override
     protected void acceptImpl(final ExcelCommands excelCommands, final CommandResult result) {
         excelCommands.getExcelFile().forEach(excelFile -> {
-            File excel = getCsvFileByPath(excelFile);
-            List<String> commands = convertFileToString(excel);
-            Source source = getSource(commands);
-            postgresSqlOperation.apply(source, excelCommands.getAlias());
+            applyExcelQueriesOrThrow(excelFile, excelCommands);
         });
+    }
+
+    private void applyExcelQueriesOrThrow(final String excelFile, final ExcelCommands excelCommands) {
+        try {
+            List<String> queries = getQueries(excelFile);
+            postgresSqlOperation.apply(new ListSource(queries), excelCommands.getAlias());
+        } catch (Exception ioe) {
+            throw new RuntimeException("Can't create workbook class. Please, check "
+                    + "if the extension of the file is correct (.xls or .xlsx)");
+        }
+    }
+
+    private List<String> getQueries(final String excelFile) {
+        Workbook workbook = getWorkbook(excelFile);
+        List<String> queries = new ArrayList<>();
+        for (int i = 0; i < workbook.getNumberOfSheets(); i++) {
+            populateQueries(workbook.getSheetAt(i), queries);
+        }
+        return queries;
+    }
+
+    private Workbook getWorkbook(final String excelFile) {
+        File file = getCsvFileByPath(excelFile);
+        try {
+            if (file.toString().endsWith(XLSX_EXTENSION)) {
+                return new XSSFWorkbook(new FileInputStream(file));
+            }
+            return new HSSFWorkbook(new FileInputStream(file));
+        } catch (IOException exception) {
+            throw new RuntimeException("Can't find the file or can't signature of the excel "
+                    + "file isn't correct. Please, create your excel file from an application "
+                    + "or from google.");
+        }
     }
 
     private File getCsvFileByPath(final String pathToFile) {
@@ -50,39 +88,59 @@ public class ExcelInterpreter extends AbstractInterpreter<ExcelCommands> {
         return fileSearcher.search(excelFolder, pathToFile);
     }
 
-    private List<String> convertFileToString(final File excelFile) {
-        try {
-            return Files.readAllLines(excelFile.toPath());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    private void populateQueries(final Sheet sheet, final List<String> queries) {
+        for (Row row : sheet) {
+            String values = getValues(row);
+            String tableName = getTableName(row);
+            queries.add(String.format(SQL_INSERT, tableName, values));
         }
     }
 
-    private Source getSource(final List<String> commands) {
-        List<String> queries = commands.stream().map(command -> {
-            String tableName = getTableName(command);
-            String values = getValues(command);
-            if (tableName.isEmpty() || values.isEmpty()) {
-                throw new IllegalArgumentException("The syntax of query isn't correct.");
+    private String getValues(final Row row) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 1; i < row.getLastCellNum(); i++) {
+            Cell cell = row.getCell(i);
+            String cellValue = getCellValue(cell);
+            if (i == row.getLastCellNum() - 1) {
+                builder.append(cellValue);
+            } else {
+                builder.append(cellValue).append(DelimiterConstant.COMA);
             }
-            return String.format(SQL_INSERT, tableName, values);
-        }).collect(Collectors.toList());
-        return new ListSource(queries);
+        }
+        return String.format(SQL_VALUES, builder.toString());
     }
 
-    private String getTableName(final String command) {
-        String tableName = command.substring(0, command.indexOf(DelimiterConstant.COMA));
-        if (tableName.isEmpty()) {
-            return DelimiterConstant.EMPTY;
+    private String getTableName(final Row row) {
+        if (row.getCell(0) != null) {
+            return getCellValue(row.getCell(0))
+                    .replaceAll(DelimiterConstant.APOSTROPHE, DelimiterConstant.EMPTY);
         }
-        return tableName;
+        return DelimiterConstant.EMPTY;
     }
 
-    private String getValues(final String values) {
-        String line = values.substring(values.indexOf(DelimiterConstant.COMA) + 1);
-        if (line.isEmpty()) {
-            return DelimiterConstant.EMPTY;
+    private String getCellValue(final Cell cell) {
+        switch (cell.getCellType()) {
+            case NUMERIC:
+                return String.valueOf(cell.getNumericCellValue());
+            case STRING:
+                return processStringValue(cell);
+            case BOOLEAN:
+                return String.valueOf(cell.getBooleanCellValue());
+            default:
+                throw new IllegalArgumentException("A cell doesn't have any corresponds cell type");
         }
-        return String.format(SQL_VALUES, String.join(DelimiterConstant.EMPTY, line));
+    }
+
+    private String processStringValue(final Cell cell) {
+        String cellValue = cell.getStringCellValue();
+        if (cellValue.equals(EMPTY_CELL)) {
+            return DelimiterConstant.APOSTROPHE + DelimiterConstant.APOSTROPHE;
+        } else if (cellValue.equals(NULL_CELL)) {
+            return cell.getStringCellValue()
+                    .replaceFirst(DelimiterConstant.APOSTROPHE, DelimiterConstant.EMPTY);
+        } else if (cellValue.endsWith(DelimiterConstant.APOSTROPHE)) {
+            return DelimiterConstant.APOSTROPHE + cell.getStringCellValue();
+        }
+        return DelimiterConstant.APOSTROPHE + cell.getStringCellValue() + DelimiterConstant.APOSTROPHE;
     }
 }

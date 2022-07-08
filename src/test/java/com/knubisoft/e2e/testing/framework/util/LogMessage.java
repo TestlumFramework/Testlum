7662b163af6d4971e1f673f61d1dc8e05f7f40bd
Darina Berveniuk <d.barveniuk@knubisoft.com>
2022-07-08 20:30:10 +0300

    change message

--- src/test/java/com/knubisoft/e2e/testing/framework/util/LogMessage.java
+++ src/test/java/com/knubisoft/e2e/testing/framework/util/LogMessage.java
@@ -231,7 +231,8 @@ public final class LogMessage {
     public static final String REPEAT_INFO = "Repeat UI commands %d times";
 
     public static final String CREDENTIALS_LOG = format(TABLE_FORMAT, "Credentials", "{}");
-    public static final String AUTH_WAS_NOT_DEFINED = "Authentication strategy was not defined in global-confige file";
+    public static final String AUTH_WAS_NOT_DEFINED = "Authentication strategy has not been defined in the "
+            + "configuration file. Usage example: <auth strategy=“basic”.../>";
 
     public static final String LOCAL_STORAGE_KEY = format(TABLE_FORMAT, "Local storage key", "{}");
     public static final String CLEAR_COOKIES_AFTER = format(TABLE_FORMAT, "Clear cookies after", "{}");

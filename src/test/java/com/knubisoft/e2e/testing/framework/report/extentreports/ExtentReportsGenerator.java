ec5d080018d71917cce49c6f74071f34f7da3719
Ivan Doroshenko <ivan_d@knubisoft.uk>
2022-07-11 18:24:08 +0300

    Added full stacktrace

--- src/test/java/com/knubisoft/e2e/testing/framework/report/extentreports/ExtentReportsGenerator.java
+++ src/test/java/com/knubisoft/e2e/testing/framework/report/extentreports/ExtentReportsGenerator.java
@@ -224,7 +224,7 @@ public class ExtentReportsGenerator implements ReportGenerator {
         if (stepExecutionInfo.isSuccess()) {
             extentTest.pass(MarkupHelper.createLabel(SUCCESS_SCENARIO_MESSAGE, ExtentColor.GREEN));
         } else {
-            extentTest.fail(stepExecutionInfo.getException().getMessage());
+            extentTest.fail(stepExecutionInfo.getException().);
         }
         extentTest.info(format(STEP_EXECUTION_TIME_TEMPLATE, stepExecutionInfo.getExecutionTime()));
     }

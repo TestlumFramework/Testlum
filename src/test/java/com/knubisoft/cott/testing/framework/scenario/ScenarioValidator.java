9a4ca01e4583e3e8a9bc7fcf67d570b596b8ba70
Darina Berveniuk <d.barveniuk@knubisoft.com>
2022-08-16 18:23:12 +0300

    add integration`s alias validation

--- src/test/java/com/knubisoft/cott/testing/framework/scenario/ScenarioValidator.java
+++ src/test/java/com/knubisoft/cott/testing/framework/scenario/ScenarioValidator.java
@@ -1,14 +1,13 @@
 package com.knubisoft.cott.testing.framework.scenario;
 
 import com.knubisoft.cott.testing.framework.configuration.GlobalTestConfigurationProvider;
-import com.knubisoft.cott.testing.framework.context.NameToAdapterAlias;
 import com.knubisoft.cott.testing.framework.exception.DefaultFrameworkException;
 import com.knubisoft.cott.testing.framework.util.SendGridUtil;
 import com.knubisoft.cott.testing.framework.validator.XMLValidator;
 import com.knubisoft.cott.testing.framework.util.FileSearcher;
 import com.knubisoft.cott.testing.framework.util.HttpUtil;
+import com.knubisoft.cott.testing.model.global_config.Integration;
 import com.knubisoft.cott.testing.model.global_config.Integrations;
-import com.knubisoft.cott.testing.model.global_config.Rabbitmq;
 import com.knubisoft.cott.testing.model.scenario.AbstractCommand;
 import com.knubisoft.cott.testing.model.scenario.Auth;
 import com.knubisoft.cott.testing.model.scenario.Body;
@@ -41,7 +40,6 @@ import com.knubisoft.cott.testing.model.scenario.Sqs;
 import com.knubisoft.cott.testing.model.scenario.StorageName;
 import com.knubisoft.cott.testing.model.scenario.Ui;
 import com.knubisoft.cott.testing.model.scenario.When;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 
 import java.io.File;
@@ -57,13 +55,9 @@ import java.util.stream.Stream;
 import static com.knubisoft.cott.testing.framework.constant.ExceptionMessage.ALIAS_NOT_FOUND;
 import static com.knubisoft.cott.testing.framework.constant.ExceptionMessage.API_NOT_FOUND;
 import static com.knubisoft.cott.testing.framework.constant.MigrationConstant.JSON_EXTENSION;
-import static com.knubisoft.cott.testing.framework.util.ResultUtil.POSTGRES;
-import static java.lang.String.format;
 
 public class ScenarioValidator implements XMLValidator<Scenario> {
 
-    @Autowired(required = false)
-    private NameToAdapterAlias nameToAdapterAlias;
 
     private final Map<AbstractCommandPredicate, AbstractCommandValidator> abstractCommandValidatorsMap;
 
@@ -100,11 +94,13 @@ public class ScenarioValidator implements XMLValidator<Scenario> {
 
         validatorMap.put(o -> o instanceof Elasticsearch, (xmlFile, command) -> {
             Elasticsearch elasticsearch = (Elasticsearch) command;
+            validateAlias(integrations.getElasticsearchIntegration().getElasticsearch(), elasticsearch.getAlias());
             validateElasticsearchCommand(xmlFile, elasticsearch);
         });
 
         validatorMap.put(o -> o instanceof S3, (xmlFile, command) -> {
             S3 s3 = (S3) command;
+            validateAlias(integrations.getS3Integration().getS3(), s3.getAlias());
             validateS3Command(xmlFile, s3);
         });
 
@@ -115,56 +111,67 @@ public class ScenarioValidator implements XMLValidator<Scenario> {
 
         validatorMap.put(o -> o instanceof Postgres, (xmlFile, command) -> {
             Postgres postgres = (Postgres) command;
+            validateAlias(integrations.getPostgresIntegration().getPostgres(), postgres.getAlias());
             validateFileIfExist(xmlFile, postgres.getFile());
         });
 
         validatorMap.put(o -> o instanceof Mysql, (xmlFile, command) -> {
             Mysql mysql = (Mysql) command;
+            validateAlias(integrations.getMysqlIntegration().getMysql(), mysql.getAlias());
             validateFileIfExist(xmlFile, mysql.getFile());
         });
 
         validatorMap.put(o -> o instanceof Oracle, (xmlFile, command) -> {
             Oracle oracle = (Oracle) command;
+            validateAlias(integrations.getOracleIntegration().getOracle(), oracle.getAlias());
             validateFileIfExist(xmlFile, oracle.getFile());
         });
 
         validatorMap.put(o -> o instanceof Redis, (xmlFile, command) -> {
             Redis redis = (Redis) command;
+            validateAlias(integrations.getRedisIntegration().getRedis(), redis.getAlias());
             validateFileIfExist(xmlFile, redis.getFile());
         });
 
         validatorMap.put(o -> o instanceof Mongo, (xmlFile, command) -> {
             Mongo mongo = (Mongo) command;
+            validateAlias(integrations.getMongoIntegration().getMongo(), mongo.getAlias());
             validateFileIfExist(xmlFile, mongo.getFile());
         });
 
         validatorMap.put(o -> o instanceof Dynamo, (xmlFile, command) -> {
             Dynamo dynamo = (Dynamo) command;
+            validateAlias(integrations.getDynamoIntegration().getDynamo(), dynamo.getAlias());
             validateFileIfExist(xmlFile, dynamo.getFile());
         });
 
         validatorMap.put(o -> o instanceof Clickhouse, (xmlFile, command) -> {
             Clickhouse clickhouse = (Clickhouse) command;
+            validateAlias(integrations.getClickhouseIntegration().getClickhouse(), clickhouse.getAlias());
             validateFileIfExist(xmlFile, clickhouse.getFile());
         });
 
         validatorMap.put(o -> o instanceof Rabbit, (xmlFile, command) -> {
             Rabbit rabbit = (Rabbit) command;
+            validateAlias(integrations.getRabbitmqIntegration().getRabbitmq(), rabbit.getAlias());
             validateRabbitCommand(xmlFile, rabbit);
         });
 
         validatorMap.put(o -> o instanceof Kafka, (xmlFile, command) -> {
             Kafka kafka = (Kafka) command;
+            validateAlias(integrations.getKafkaIntegration().getKafka(), kafka.getAlias());
             validateKafkaCommand(xmlFile, kafka);
         });
 
         validatorMap.put(o -> o instanceof Sqs, (xmlFile, command) -> {
            Sqs sqs = (Sqs) command;
+           validateAlias(integrations.getSqsIntegration().getSqs(), sqs.getAlias());
            validateSqsCommand(xmlFile, sqs);
         });
 
         validatorMap.put(o -> o instanceof Sendgrid, (xmlFile, command) -> {
             Sendgrid sendgrid = (Sendgrid) command;
+            validateAlias(integrations.getSendgridIntegration().getSendgrid(), sendgrid.getAlias());
             validateSendgridCommand(xmlFile, sendgrid);
         });
 
@@ -188,16 +195,12 @@ public class ScenarioValidator implements XMLValidator<Scenario> {
         }
     }
 
-    private void validate(final Postgres postgres) {
-        nameToAdapterAlias.getByNameOrThrow(POSTGRES );
+    private void validateAlias(final List<? extends Integration> integrationsList, final String alias) {
+        integrationsList.stream().filter(Integration::isEnabled).filter(o -> o.getAlias().equals(alias))
+                .findFirst().orElseThrow(() -> new DefaultFrameworkException(ALIAS_NOT_FOUND, alias));
     }
 
     private void validateElasticsearchCommand(final File xmlFile, final Elasticsearch elasticsearch) {
-        integrations.getElasticsearchIntegration().getElasticsearch().stream()
-                .filter(com.knubisoft.cott.testing.model.global_config.Elasticsearch::isEnabled)
-                .filter(s -> s.getAlias().equals(elasticsearch.getAlias())).findFirst()
-                .orElseThrow(() -> new DefaultFrameworkException(ALIAS_NOT_FOUND, elasticsearch.getAlias()));
-
         Stream.of(elasticsearch.getPost(), elasticsearch.getGet(), elasticsearch.getPut(), elasticsearch.getDelete())
                 .filter(Objects::nonNull)
                 .filter(v -> StringUtils.hasText(v.getResponse().getFile()))
@@ -205,22 +208,12 @@ public class ScenarioValidator implements XMLValidator<Scenario> {
     }
 
     private void validateSqsCommand(final File xmlFile, final Sqs sqs) {
-        integrations.getSqsIntegration().getSqs().stream()
-                .filter(com.knubisoft.cott.testing.model.global_config.Sqs::isEnabled)
-                .filter(s -> s.getAlias().equals(sqs.getAlias())).findFirst()
-                .orElseThrow(() -> new DefaultFrameworkException(ALIAS_NOT_FOUND, sqs.getAlias()));
-
         Stream.of(sqs.getReceive(), sqs.getSend())
                 .filter(StringUtils::hasText).filter(o -> o.endsWith(JSON_EXTENSION))
                 .forEach(v -> FileSearcher.searchFileFromDir(xmlFile, v));
     }
 
     private void validateRabbitCommand(final File xmlFile, final Rabbit rabbit) {
-        integrations.getRabbitmqIntegration().getRabbitmq().stream()
-                .filter(Rabbitmq::isEnabled)
-                .filter(s -> s.getAlias().equals(rabbit.getAlias())).findFirst()
-                .orElseThrow(() -> new DefaultFrameworkException(ALIAS_NOT_FOUND, rabbit.getAlias()));
-
         rabbit.getSendOrReceive().stream().filter(command -> command instanceof SendRmqMessage)
                 .filter(send -> StringUtils.hasText(((SendRmqMessage) send).getFile()))
                 .forEach(send -> FileSearcher.searchFileFromDir(xmlFile, ((SendRmqMessage) send).getFile()));
@@ -231,11 +224,6 @@ public class ScenarioValidator implements XMLValidator<Scenario> {
     }
 
     private void validateKafkaCommand(final File xmlFile, final Kafka kafka) {
-        integrations.getKafkaIntegration().getKafka().stream()
-                .filter(com.knubisoft.cott.testing.model.global_config.Kafka::isEnabled)
-                .filter(s -> s.getAlias().equals(kafka.getAlias())).findFirst()
-                .orElseThrow(() -> new DefaultFrameworkException(ALIAS_NOT_FOUND, kafka.getAlias()));
-
         kafka.getSendOrReceive().stream().filter(command -> command instanceof SendKafkaMessage)
                 .filter(command -> StringUtils.hasText(((SendKafkaMessage) command).getFile()))
                 .forEach(command -> FileSearcher.searchFileFromDir(xmlFile, ((SendKafkaMessage) command).getFile()));
@@ -252,22 +240,12 @@ public class ScenarioValidator implements XMLValidator<Scenario> {
     }
 
     private void validateS3Command(final File xmlFile, final S3 s3) {
-        integrations.getS3Integration().getS3().stream()
-                .filter(com.knubisoft.cott.testing.model.global_config.S3::isEnabled)
-                .filter(s -> s.getAlias().equals(s3.getAlias())).findFirst()
-                .orElseThrow(() -> new DefaultFrameworkException(ALIAS_NOT_FOUND, s3.getAlias()));
-
         Stream.of(s3.getDownload(), s3.getUpload())
                 .filter(StringUtils::hasText)
                 .forEach(v -> FileSearcher.searchFileFromDir(xmlFile, v));
     }
 
     private void validateSendgridCommand(final File xmlFile, final Sendgrid sendgrid) {
-        integrations.getSendgridIntegration().getSendgrid().stream()
-                .filter(com.knubisoft.cott.testing.model.global_config.Sendgrid::isEnabled)
-                .filter(s -> s.getAlias().equals(sendgrid.getAlias())).findFirst()
-                .orElseThrow(() -> new DefaultFrameworkException(ALIAS_NOT_FOUND, sendgrid.getAlias()));
-
         SendgridInfo sendgridInfo = SendGridUtil.getSendgridMethodMetadata(sendgrid).getHttpInfo();
         Response response = sendgridInfo.getResponse();
         if (response != null && response.getFile() != null) {
@@ -277,8 +255,7 @@ public class ScenarioValidator implements XMLValidator<Scenario> {
         SendgridWithBody commandWithBody = (SendgridWithBody) sendgridInfo;
         Body body = commandWithBody.getBody();
         if (body != null && body.getFrom() != null) {
-            String fileName = body.getFrom().getFile();
-            FileSearcher.searchFileFromDir(xmlFile, fileName);
+            FileSearcher.searchFileFromDir(xmlFile, body.getFrom().getFile());
         }
 
     }
@@ -350,15 +327,6 @@ public class ScenarioValidator implements XMLValidator<Scenario> {
 //                throw new DefaultFrameworkException(format(FAILED_CONNECTION_TO_DATABASE,
 //                        postgresResult.getDatabaseName()));
 //            }
-//        }
-//    }
-//
-//    //CHECKSTYLE:ON
-//
-//    private void validateExistsLocator(final CommandWithLocator command) {
-//        String id = command.getLocatorId();
-//        if (id != null) {
-//            GlobalLocators.getInstance().getLocator(id);
 //        }
 //    }
 //

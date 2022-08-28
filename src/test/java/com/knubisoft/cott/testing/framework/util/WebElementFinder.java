953a1f3e87f29c4391f61e7833b10c50076d923d
Ivan Doroshenko <ivan_d@knubisoft.uk>
2022-08-28 12:53:45 +0300

    Refactored

--- src/test/java/com/knubisoft/cott/testing/framework/util/WebElementFinder.java
+++ src/test/java/com/knubisoft/cott/testing/framework/util/WebElementFinder.java
@@ -2,6 +2,7 @@ package com.knubisoft.cott.testing.framework.util;
 
 import com.knubisoft.cott.testing.framework.exception.DefaultFrameworkException;
 import com.knubisoft.cott.testing.model.pages.Locator;
+import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -10,6 +11,7 @@ import org.openqa.selenium.WebElement;
 import static java.lang.String.format;
 
 @Slf4j
+@UtilityClass
 public final class WebElementFinder {
 
     public WebElement find(final Locator locator, final WebDriver driver) {

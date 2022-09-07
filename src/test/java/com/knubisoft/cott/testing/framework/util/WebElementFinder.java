bcf2e8e5fa599b1923f82e6200e2470b377485c1
borislavmarusov <b.marusov@knubisoft.com>
2022-09-07 14:45:12 +0300

    Another refactor of method find() in WebElementFinder class

--- src/test/java/com/knubisoft/cott/testing/framework/util/WebElementFinder.java
+++ src/test/java/com/knubisoft/cott/testing/framework/util/WebElementFinder.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
@@ -19,18 +20,21 @@ import static java.lang.String.format;
 @UtilityClass
 public final class WebElementFinder {
 
-    private static final Map<Predicate<Locator>, Function<Locator, By>> CHOOSESELECTOR =
-            ImmutableMap.<Predicate<Locator>, Function<Locator, By>>builder()
-                    .put(it -> it.getXpath() != null, it -> By.xpath(it.getXpath()))
-                    .put(it -> it.getId() != null, it -> By.id(it.getId()))
-                    .put(it -> it.getClazz() != null, it -> By.className(it.getClazz()))
-                    .put(it -> it.getCssSelector() != null, it -> By.cssSelector(it.getCssSelector()))
-                    .put(it -> it.getLinkText() != null, it -> By.linkText(it.getLinkText()))
-                    .put(it -> it.getPartialLinkText() != null, it -> By.partialLinkText(it.getPartialLinkText()))
-                    .build();
+    private static final Map<Predicate<Locator>, Function<Locator, By>> SEARCH_TYPES;
+
+    static {
+        SEARCH_TYPES = ImmutableMap.<Predicate<Locator>, Function<Locator, By>>builder()
+                .put(it -> Objects.nonNull(it.getXpath()), it -> By.xpath(it.getXpath()))
+                .put(it -> Objects.nonNull(it.getId()), it -> By.id(it.getId()))
+                .put(it -> Objects.nonNull(it.getClazz()), it -> By.className(it.getClazz()))
+                .put(it -> Objects.nonNull(it.getCssSelector()), it -> By.cssSelector(it.getCssSelector()))
+                .put(it -> Objects.nonNull(it.getLinkText()), it -> By.linkText(it.getLinkText()))
+                .put(it -> Objects.nonNull(it.getPartialLinkText()), it -> By.partialLinkText(it.getPartialLinkText()))
+                .build();
+    }
 
     public WebElement find(final Locator locator, final WebDriver driver) {
-        return CHOOSESELECTOR.entrySet().stream()
+        return SEARCH_TYPES.entrySet().stream()
                 .filter(it -> it.getKey().test(locator))
                 .findFirst()
                 .map(it -> it.getValue().apply(locator))

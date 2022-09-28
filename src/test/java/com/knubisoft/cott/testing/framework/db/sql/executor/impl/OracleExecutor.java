5cff1704cb48090a40b383f625caacc68b754fdf
Alexey Cherepashkin <a.cherepashkin@knubisoft.com>
2022-09-28 17:21:52 +0300

    Temp fix

--- src/test/java/com/knubisoft/cott/testing/framework/db/sql/executor/impl/OracleExecutor.java
+++ src/test/java/com/knubisoft/cott/testing/framework/db/sql/executor/impl/OracleExecutor.java
@@ -10,18 +10,20 @@ import static java.util.Objects.requireNonNull;
 
 public class OracleExecutor extends AbstractSqlExecutor {
 
-    private static final String DISABLE_FOREIGN_KEYS = "CALL DISABLE_FOREIGN_KEYS()";
-    private static final String DISABLE_PRIMARY_KEYS = "CALL DISABLE_PRIMARY_KEYS()";
-    private static final String TRUNCATE_TABLES = "CALL TRUNCATE_TABLES()";
-    private static final String ENABLE_PRIMARY_KEYS = "CALL ENABLE_PRIMARY_KEYS()";
-    private static final String ENABLE_FOREIGN_KEYS = "CALL ENABLE_FOREIGN_KEYS()";
+    private static final String SELECT_ORACLE_TABLE_NAMES = "SELECT table_name FROM all_tables WHERE owner = '%s'";
+    private static final String DISABLE_ALL_TRIGGERS = "ALTER TABLE %s DISABLE ALL TRIGGERS";
+    private static final String DISABLE_VALIDATE_PRIMARY_KEY = "ALTER TABLE %s DISABLE PRIMARY KEY";
+    private static final String TRUNCATE_TABLES = "TRUNCATE TABLE %s";
+    private static final String ENABLE_VALIDATE_PRIMARY_KEY = "ALTER TABLE %s ENABLE PRIMARY KEY";
+    private static final String ENABLE_ALL_TRIGGERS = "ALTER TABLE %s ENABLE ALL TRIGGERS";
 
+    //todo
     private static final List<String> TRUNCATE_QUERIES = Arrays.asList(
-            DISABLE_FOREIGN_KEYS,
-            DISABLE_PRIMARY_KEYS,
+            DISABLE_ALL_TRIGGERS,
+            DISABLE_VALIDATE_PRIMARY_KEY,
             TRUNCATE_TABLES,
-            ENABLE_PRIMARY_KEYS,
-            ENABLE_FOREIGN_KEYS);
+            ENABLE_VALIDATE_PRIMARY_KEY,
+            ENABLE_ALL_TRIGGERS);
 
     public OracleExecutor(final DataSource dataSource) {
         super(dataSource);
@@ -29,8 +31,43 @@ public class OracleExecutor extends AbstractSqlExecutor {
 
     @Override
     public void truncate() {
-        for (String query : TRUNCATE_QUERIES) {
-            requireNonNull(template).execute(query);
-        }
+        //todo
+        requireNonNull(template).execute("declare\n" +
+                "begin\n" +
+                "    for c1 in (select y1.table_name, y1.constraint_name\n" +
+                "               from user_constraints y1,\n" +
+                "                    user_tables x1\n" +
+                "               where x1.table_name = y1.table_name\n" +
+                "               order by y1.r_constraint_name nulls last)\n" +
+                "        loop\n" +
+                "            begin\n" +
+                "                dbms_output.put_line('alter table ' || c1.table_name || ' disable constraint ' || c1.constraint_name ||\n" +
+                "                                     ';');\n" +
+                "                execute immediate ('alter table ' || c1.table_name || ' disable constraint ' || c1.constraint_name);\n" +
+                "            end;\n" +
+                "        end loop;\n" +
+                "\n" +
+                "    for t1 in (select table_name from user_tables)\n" +
+                "        loop\n" +
+                "            begin\n" +
+                "                dbms_output.put_line('truncate table ' || t1.table_name || ';');\n" +
+                "                execute immediate ('truncate table ' || t1.table_name);\n" +
+                "            end;\n" +
+                "        end loop;\n" +
+                "\n" +
+                "    for c2 in (select y2.table_name, y2.constraint_name\n" +
+                "               from user_constraints y2,\n" +
+                "                    user_tables x2\n" +
+                "               where x2.table_name = y2.table_name\n" +
+                "               order by y2.r_constraint_name nulls first)\n" +
+                "        loop\n" +
+                "            begin\n" +
+                "                dbms_output.put_line('alter table ' || c2.table_name || ' enable constraint ' || c2.constraint_name ||\n" +
+                "                                     ';');\n" +
+                "                execute immediate ('alter table ' || c2.table_name || ' enable constraint ' || c2.constraint_name);\n" +
+                "            end;\n" +
+                "        end loop;\n" +
+                "\n" +
+                "end;");
     }
 }

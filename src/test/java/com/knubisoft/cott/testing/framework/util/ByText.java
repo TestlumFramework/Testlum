8e51113022f001760c1ad1aa225ecb95da2415bf
borislavmarusov <b.marusov@knubisoft.com>
2022-09-28 14:20:43 +0300

    Add search locator by partial text, add exception

--- src/test/java/com/knubisoft/cott/testing/framework/util/ByText.java
+++ src/test/java/com/knubisoft/cott/testing/framework/util/ByText.java
@@ -1,5 +1,7 @@
 package com.knubisoft.cott.testing.framework.util;
 
+import com.knubisoft.cott.testing.framework.constant.ExceptionMessage;
+import com.knubisoft.cott.testing.framework.exception.DefaultFrameworkException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebElement;
@@ -12,8 +14,11 @@ public class ByText {
         return new By() {
             @Override
             public List<WebElement> findElements(SearchContext context) {
-                String xpath = "//*[text()='" + text + "']";
+                String xpath = "//*[contains(text(), '" + text + "')]";
                 List<WebElement> elements = context.findElements(By.xpath(xpath));
+                if (elements.size() > 1){
+                    throw new DefaultFrameworkException(ExceptionMessage.FOUND_MORE_THEN_ONE_ELEMENT, text);
+                }
                 return elements;
             }
         };

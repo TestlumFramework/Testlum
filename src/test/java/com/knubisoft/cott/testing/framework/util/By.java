0fb7d36f019db42460da86ead83cb62b4b0a82ce
borislavmarusov <b.marusov@knubisoft.com>
2022-09-29 18:05:05 +0300

    Refactor By, add comment

--- src/test/java/com/knubisoft/cott/testing/framework/util/By.java
+++ src/test/java/com/knubisoft/cott/testing/framework/util/By.java
@@ -12,21 +12,8 @@ import java.util.List;
 @UtilityClass
 public class By {
 
-    private static final String XPATH_WITH_TEXT_METHOD = "//*[contains(text(), '%s')]";
+    private static final String XPATH_TEMPLATE_FOR_TEXT_SEARCH = "//*[contains(text(), '%s')]";
 
-    public static org.openqa.selenium.By text(final String text) {
-        return new org.openqa.selenium.By() {
-            @Override
-            public List<WebElement> findElements(final SearchContext context) {
-                String xpath = String.format(XPATH_WITH_TEXT_METHOD, text);
-                List<WebElement> elements = context.findElements(By.xpath(xpath));
-                if (elements.size() > 1) {
-                    throw new DefaultFrameworkException(ExceptionMessage.FOUND_MORE_THEN_ONE_ELEMENT, text);
-                }
-                return elements;
-            }
-        };
-    }
     public org.openqa.selenium.By xpath(final String xpath) {
         return org.openqa.selenium.By.xpath(xpath);
     }
@@ -35,27 +22,29 @@ public class By {
         return org.openqa.selenium.By.id(id);
     }
 
-    public org.openqa.selenium.By linkText(final String linkText) {
-        return org.openqa.selenium.By.linkText(linkText);
-    }
-
-    public org.openqa.selenium.By partialLinkText(final String partialLinkText) {
-        return org.openqa.selenium.By.partialLinkText(partialLinkText);
-    }
-
-    public org.openqa.selenium.By name(final String name) {
-        return org.openqa.selenium.By.name(name);
-    }
-
-    public org.openqa.selenium.By tagName(final String tagName) {
-        return org.openqa.selenium.By.tagName(tagName);
-    }
-
     public org.openqa.selenium.By className(final String className) {
         return org.openqa.selenium.By.className(className);
     }
 
     public org.openqa.selenium.By cssSelector(final String cssSelector) {
         return org.openqa.selenium.By.cssSelector(cssSelector);
+    }
+
+    /*The check "elements.size() > 1" is made in order for the tester to indicate exactly
+    which of the elements with the given text
+    he wants to use, in another way (for example by xpath or id)*/
+
+    public static org.openqa.selenium.By text(final String text) {
+        return new org.openqa.selenium.By() {
+            @Override
+            public List<WebElement> findElements(final SearchContext context) {
+                String xpath = String.format(XPATH_TEMPLATE_FOR_TEXT_SEARCH, text);
+                List<WebElement> elements = context.findElements(By.xpath(xpath));
+                if (elements.size() > 1) {
+                    throw new DefaultFrameworkException(ExceptionMessage.FOUND_MORE_THEN_ONE_ELEMENT, text);
+                }
+                return elements;
+            }
+        };
     }
 }

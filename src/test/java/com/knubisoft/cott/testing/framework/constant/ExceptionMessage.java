b220f49d44649f34f742def16614700f7394d7c7
borislavmarusov <b.marusov@knubisoft.com>
2022-10-21 17:33:47 +0300

    Add possibility to use compound classes

--- src/test/java/com/knubisoft/cott/testing/framework/constant/ExceptionMessage.java
+++ src/test/java/com/knubisoft/cott/testing/framework/constant/ExceptionMessage.java
@@ -119,7 +119,7 @@ public class ExceptionMessage {
     public static final String VAR_QUERY_RESULT_ERROR =
             "Query result is empty. Perhaps your database is empty or there is no such value in the database";
     public static final String WEB_ELEMENT_ATTRIBUTE_NOT_EXIST = "Web element does not have attribute <%s>";
-    public static final String FOUND_MORE_THEN_ONE_ELEMENT = "More than one element was found by text <%s>";
+    public static final String FOUND_MORE_THEN_ONE_ELEMENT = "More than one element was found by locator <%s>";
     public static final String SCROLL_TYPE_NOT_FOUND = "Scroll type <%s> not supported";
     public static final String NO_LOCATOR_FOUND_FOR_INNER_SCROLL = "You didn't specify a locator for the inner scroll";
     public static final String INVALID_LOCATOR =
--- src/test/java/com/knubisoft/cott/testing/framework/util/By.java
+++ src/test/java/com/knubisoft/cott/testing/framework/util/By.java
@@ -10,11 +10,14 @@ import org.openqa.selenium.WebElement;
 
 import java.util.List;
 
+import static java.lang.String.format;
+
 @UtilityClass
 public class By {
 
     private static final String XPATH_TEMPLATE_FOR_TEXT_SEARCH = "//*[contains(text(), '%s')]";
     private static final String XPATH_TEMPLATE_FOR_TEXT_SEARCH_FROM_PLACEHOLDER = "//*[@placeholder='%s']";
+    private static final String XPATH_TEMPLATE_FOR_CLASS_NAME_SEARCH = "//*[@class='%s']";
 
     public org.openqa.selenium.By xpath(final String xpath) {
         return org.openqa.selenium.By.xpath(xpath);
@@ -24,12 +27,18 @@ public class By {
         return org.openqa.selenium.By.id(id);
     }
 
-    public org.openqa.selenium.By className(final String className) {
-        return org.openqa.selenium.By.className(className);
-    }
-
     public org.openqa.selenium.By cssSelector(final String cssSelector) {
         return org.openqa.selenium.By.cssSelector(cssSelector);
+    }
+
+    public org.openqa.selenium.By className(final String className) {
+        return new org.openqa.selenium.By() {
+            @Override
+            public List<WebElement> findElements(SearchContext context) {
+                String xpathWithClassName = format(XPATH_TEMPLATE_FOR_CLASS_NAME_SEARCH, className);
+                return validateElements(xpathWithClassName, context, className);
+            }
+        };
     }
 
     /*COTT tool restricts the user and prevents the use of text
@@ -40,14 +49,18 @@ public class By {
             @Override
             public List<WebElement> findElements(final SearchContext context) {
                 String xpath = text.isPlaceholder()
-                        ? String.format(XPATH_TEMPLATE_FOR_TEXT_SEARCH_FROM_PLACEHOLDER, text.getValue())
-                        : String.format(XPATH_TEMPLATE_FOR_TEXT_SEARCH, text.getValue());
-                List<WebElement> elements = context.findElements(By.xpath(xpath));
-                if (elements.size() > 1) {
-                    throw new DefaultFrameworkException(ExceptionMessage.FOUND_MORE_THEN_ONE_ELEMENT, text.getValue());
-                }
-                return elements;
+                        ? format(XPATH_TEMPLATE_FOR_TEXT_SEARCH_FROM_PLACEHOLDER, text.getValue())
+                        : format(XPATH_TEMPLATE_FOR_TEXT_SEARCH, text.getValue());
+                return validateElements(xpath, context, text.getValue());
             }
         };
+    }
+
+    private List<WebElement> validateElements(final String xpath, final SearchContext context, final String locator) {
+         List<WebElement> elements  = context.findElements(By.xpath(xpath));
+         if (elements.size() > 1) {
+             throw new DefaultFrameworkException(ExceptionMessage.FOUND_MORE_THEN_ONE_ELEMENT, locator);
+         }
+         return elements;
     }
 }

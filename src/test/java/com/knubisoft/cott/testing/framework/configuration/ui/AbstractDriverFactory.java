3c9d55bb6f50e327b15b7dc3e5f0b66c2a6aa07f
Darina Berveniuk <d.barveniuk@knubisoft.com>
2022-10-27 12:32:00 +0300

    remove abstract driver factory

--- src/test/java/com/knubisoft/cott/testing/framework/configuration/ui/AbstractDriverFactory.java
+++
@@ -1,7 +0,0 @@
-package com.knubisoft.cott.testing.framework.configuration.ui;
-
-import org.openqa.selenium.WebDriver;
-
-public interface AbstractDriverFactory {
-    WebDriver createDriver(Object deviceOrBrowser);
-}

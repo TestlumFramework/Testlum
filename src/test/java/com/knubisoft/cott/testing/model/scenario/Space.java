dbdc723352339fa32dbc8f2739429ec4c7ef6d18
borislavmarusov <b.marusov@knubisoft.com>
2022-11-04 17:31:10 +0200

    New model classes

---
+++ src/test/java/com/knubisoft/cott/testing/model/scenario/Space.java
@@ -0,0 +1,32 @@
+
+package com.knubisoft.cott.testing.model.scenario;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for space complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="space"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;extension base="{http://www.knubisoft.com/cott/testing/model/scenario}abstractCommand"&gt;
+ *     &lt;/extension&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "space")
+public class Space
+    extends AbstractCommand
+{
+
+
+}

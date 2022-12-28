785f910d559145b7f4a9f4cb2560f64c2e2c62b9
Vladyslav Kolesnyk <v.kolesnyk@knubisoft.com>
2022-12-28 14:12:50 +0200

    added missed "copy" hotkey changes

--- src/test/java/com/knubisoft/cott/testing/model/scenario/Copy.java
+++ src/test/java/com/knubisoft/cott/testing/model/scenario/Copy.java
@@ -3,6 +3,7 @@ package com.knubisoft.cott.testing.model.scenario;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlType;
 
 
@@ -14,7 +15,8 @@ import javax.xml.bind.annotation.XmlType;
  * <pre>
  * &lt;complexType name="copy"&gt;
  *   &lt;complexContent&gt;
- *     &lt;extension base="{http://www.knubisoft.com/cott/testing/model/scenario}commandWithLocator"&gt;
+ *     &lt;extension base="{http://www.knubisoft.com/cott/testing/model/scenario}abstractUiCommand"&gt;
+ *       &lt;attribute name="locatorId" type="{http://www.knubisoft.com/cott/testing/model/scenario}scenarioLocator" /&gt;
  *     &lt;/extension&gt;
  *   &lt;/complexContent&gt;
  * &lt;/complexType&gt;
@@ -25,8 +27,34 @@ import javax.xml.bind.annotation.XmlType;
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "copy")
 public class Copy
-    extends CommandWithLocator
+    extends AbstractUiCommand
 {
 
+    @XmlAttribute(name = "locatorId")
+    protected String locatorId;
+
+    /**
+     * Gets the value of the locatorId property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getLocatorId() {
+        return locatorId;
+    }
+
+    /**
+     * Sets the value of the locatorId property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setLocatorId(String value) {
+        this.locatorId = value;
+    }
 
 }

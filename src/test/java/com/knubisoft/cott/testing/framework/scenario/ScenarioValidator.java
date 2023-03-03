baaae93780dce6be3e8754aadb21861f87accdc2
Shumskyi Mykyta <n.shumsky@knubisoft.com>
2023-03-03 16:38:16 +0200

    refactored

--- src/test/java/com/knubisoft/cott/testing/framework/scenario/ScenarioValidator.java
+++ src/test/java/com/knubisoft/cott/testing/framework/scenario/ScenarioValidator.java
@@ -599,7 +599,7 @@ public class ScenarioValidator implements XMLValidator<Scenario> {
         auth.getCommands().stream()
                 .filter(command -> command instanceof Http)
                 .map(command -> (Http) command)
-                .filter(http -> http.getAlias().equalsIgnoreCase(auth.getApiAlias()))
+                .filter(http -> http.getAlias().equals(auth.getApiAlias()))
                 .findFirst()
                 .orElseThrow(() ->
                         new DefaultFrameworkException("Alias from http command doesn't match with alias from Auth"));

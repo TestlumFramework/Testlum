fbc7da82097ffd64c634dfedd64496e86455e9b7
Shumskyi Mykyta <n.shumsky@knubisoft.com>
2023-03-20 20:16:28 +0200

    refactored

--- src/test/java/com/knubisoft/cott/testing/framework/util/ConditionUtil.java
+++ src/test/java/com/knubisoft/cott/testing/framework/util/ConditionUtil.java
@@ -4,7 +4,6 @@ import com.knubisoft.cott.testing.framework.scenario.ScenarioContext;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Locale;
 import java.util.Objects;
 
 import static com.knubisoft.cott.testing.framework.constant.LogMessage.CONDITION_LOG;
@@ -15,15 +14,8 @@ public class ConditionUtil {
 
     public boolean validateCondition(final String conditionResult,
                                      final ScenarioContext context) {
-        if (!Objects.isNull(conditionResult)) {
-            return checkConditionResult(context.getCondition(conditionResult));
-        }
-        return true;
-    }
-
-    public boolean checkConditionResult(final Boolean conditionResult) {
-        if (!conditionResult) {
-            log.info(CONDITION_LOG, String.valueOf(false).toUpperCase(Locale.ENGLISH));
+        if (!Objects.isNull(conditionResult) && !context.getCondition(conditionResult)) {
+            log.info(CONDITION_LOG, conditionResult, Boolean.FALSE);
             return false;
         }
         return true;

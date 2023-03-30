0e212135ecc0d1a03911868c0cf9ab6e77484e1c
Shumskyi Mykyta <n.shumsky@knubisoft.com>
2023-03-30 17:50:37 +0300

    add logs and reports for condition

--- src/test/java/com/knubisoft/cott/testing/framework/util/ConditionUtil.java
+++ src/test/java/com/knubisoft/cott/testing/framework/util/ConditionUtil.java
@@ -1,5 +1,6 @@
 package com.knubisoft.cott.testing.framework.util;
 
+import com.knubisoft.cott.testing.framework.report.CommandResult;
 import com.knubisoft.cott.testing.framework.scenario.ScenarioContext;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
@@ -7,16 +8,20 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.Objects;
 
 import static com.knubisoft.cott.testing.framework.constant.LogMessage.CONDITION_LOG;
+import static com.knubisoft.cott.testing.framework.util.ResultUtil.CONDITION;
 
 @Slf4j
 @UtilityClass
 public class ConditionUtil {
 
     public boolean isTrue(final String conditionName,
+                          final CommandResult result,
                           final ScenarioContext context) {
-        if (Objects.nonNull(conditionName) && !context.getCondition(conditionName)) {
-            log.info(CONDITION_LOG, conditionName);
-            return false;
+        if (Objects.nonNull(conditionName)) {
+            boolean conditionResult = context.getCondition(conditionName);
+            log.info(CONDITION_LOG, conditionName, conditionResult);
+            result.put(CONDITION, conditionName + " : " + conditionResult);
+            return conditionResult;
         }
         return true;
     }

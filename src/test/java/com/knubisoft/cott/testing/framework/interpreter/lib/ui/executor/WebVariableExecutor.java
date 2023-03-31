7f0f01ee9f8b176d97d7cda8863f46f48f08ef68
Alexey Cherepashkin <a.cherepashkin@knubisoft.com>
2023-03-31 11:22:40 +0300

    Change var/assert executors

--- src/test/java/com/knubisoft/cott/testing/framework/interpreter/lib/ui/executor/WebVariableExecutor.java
+++ src/test/java/com/knubisoft/cott/testing/framework/interpreter/lib/ui/executor/WebVariableExecutor.java
@@ -1,7 +1,6 @@
 package com.knubisoft.cott.testing.framework.interpreter.lib.ui.executor;
 
 import com.knubisoft.cott.testing.framework.constant.DelimiterConstant;
-import com.knubisoft.cott.testing.framework.exception.DefaultFrameworkException;
 import com.knubisoft.cott.testing.framework.interpreter.lib.ui.AbstractUiExecutor;
 import com.knubisoft.cott.testing.framework.interpreter.lib.ui.ExecutorDependencies;
 import com.knubisoft.cott.testing.framework.interpreter.lib.ui.ExecutorForClass;
@@ -9,23 +8,22 @@ import com.knubisoft.cott.testing.framework.report.CommandResult;
 import com.knubisoft.cott.testing.framework.util.By;
 import com.knubisoft.cott.testing.framework.util.LogUtil;
 import com.knubisoft.cott.testing.framework.util.ResultUtil;
-import com.knubisoft.cott.testing.framework.util.VariableService;
+import com.knubisoft.cott.testing.framework.util.VariableHelper;
+import com.knubisoft.cott.testing.framework.util.VariableHelper.VarMethod;
+import com.knubisoft.cott.testing.framework.util.VariableHelper.VarPredicate;
 import com.knubisoft.cott.testing.model.scenario.WebVar;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.Cookie;
 import org.springframework.beans.factory.annotation.Autowired;
-
 
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
-import java.util.function.BiFunction;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-import static com.knubisoft.cott.testing.framework.constant.LogMessage.FAILED_VARIABLE_WITH_PATH_LOG;
+import static com.knubisoft.cott.testing.framework.constant.LogMessage.FAILED_VARIABLE_LOG;
 import static com.knubisoft.cott.testing.framework.util.ResultUtil.COOKIES;
 import static com.knubisoft.cott.testing.framework.util.ResultUtil.HTML_DOM;
 import static com.knubisoft.cott.testing.framework.util.ResultUtil.NO_EXPRESSION;
@@ -37,13 +35,13 @@ import static java.util.Objects.nonNull;
 @ExecutorForClass(WebVar.class)
 public class WebVariableExecutor extends AbstractUiExecutor<WebVar> {
 
-    private final Map<WebVarFromPredicate, WebVarFromMethod> webVarToMethodMap;
+    private final Map<VarPredicate<WebVar>, VarMethod<WebVar>> varToMethodMap;
     @Autowired
-    private VariableService variableService;
+    private VariableHelper variableHelper;
 
     public WebVariableExecutor(final ExecutorDependencies dependencies) {
         super(dependencies);
-        Map<WebVarFromPredicate, WebVarFromMethod> webVarMap = new HashMap<>();
+        Map<VarPredicate<WebVar>, VarMethod<WebVar>> webVarMap = new HashMap<>();
         webVarMap.put(var -> nonNull(var.getSql()), this::getSQLResult);
         webVarMap.put(var -> nonNull(var.getFile()), this::getFileResult);
         webVarMap.put(var -> nonNull(var.getExpression()), this::getExpressionResult);
@@ -51,82 +49,71 @@ public class WebVariableExecutor extends AbstractUiExecutor<WebVar> {
         webVarMap.put(var -> nonNull(var.getCookie()), this::getWebCookiesResult);
         webVarMap.put(var -> nonNull(var.getDom()), this::getDomResult);
         webVarMap.put(var -> nonNull(var.getUrl()), this::getUrlResult);
-        webVarToMethodMap = Collections.unmodifiableMap(webVarMap);
+        varToMethodMap = Collections.unmodifiableMap(webVarMap);
     }
 
     @Override
-    public void execute(final WebVar webVar, final CommandResult result) {
+    public void execute(final WebVar var, final CommandResult result) {
         try {
-            setContextVariable(webVar, result);
+            setContextVariable(var, result);
         } catch (Exception e) {
-            log.info(FAILED_VARIABLE_WITH_PATH_LOG, webVar.getName(), webVar.getComment());
+            log.info(FAILED_VARIABLE_LOG, var.getName(), var.getComment());
             throw e;
         }
     }
 
-    private void setContextVariable(final WebVar webVar, final CommandResult result) {
-        String value = getValueForContext(webVar, result);
-        dependencies.getScenarioContext().set(webVar.getName(), value);
-        LogUtil.logVarInfo(webVar.getName(), value);
+    private void setContextVariable(final WebVar var, final CommandResult result) {
+        String value = getValueForContext(var, result);
+        dependencies.getScenarioContext().set(var.getName(), value);
+        LogUtil.logVarInfo(var.getName(), value);
     }
 
-    private String getValueForContext(final WebVar webVar, final CommandResult result) {
-        return webVarToMethodMap.keySet().stream()
-                .filter(key -> key.test(webVar))
-                .findFirst()
-                .map(webVarToMethodMap::get)
-                .orElseThrow(() -> new DefaultFrameworkException("Type of 'Var' tag is not supported"))
-                .apply(webVar, result);
+    private String getValueForContext(final WebVar var, final CommandResult result) {
+        return variableHelper.lookupVarMethod(varToMethodMap, var)
+                .apply(var, result);
     }
 
-    private String getDomResult(final WebVar webVar, final CommandResult result) {
-        String xpath = webVar.getDom().getXpath();
-        if (Objects.nonNull(xpath)) {
+    private String getDomResult(final WebVar var, final CommandResult result) {
+        String xpath = var.getDom().getXpath();
+        if (StringUtils.isNotBlank(xpath)) {
             String valueResult = dependencies.getDriver().findElement(By.xpath(xpath)).getAttribute("outerHTML");
-            ResultUtil.addVariableMetaData(HTML_DOM, webVar.getName(), xpath, valueResult, result);
+            ResultUtil.addVariableMetaData(HTML_DOM, var.getName(), xpath, valueResult, result);
             return valueResult;
         }
         String valueResult = dependencies.getDriver().getPageSource();
-        ResultUtil.addVariableMetaData(HTML_DOM, webVar.getName(), NO_EXPRESSION, valueResult, result);
+        ResultUtil.addVariableMetaData(HTML_DOM, var.getName(), NO_EXPRESSION, valueResult, result);
         return valueResult;
     }
 
-    private String getWebCookiesResult(final WebVar webVar, final CommandResult result) {
+    private String getWebCookiesResult(final WebVar var, final CommandResult result) {
         Set<Cookie> cookies = dependencies.getDriver().manage().getCookies();
         String valueResult = cookies.stream()
                 .map(cookie -> String.join(DelimiterConstant.EQUALS_MARK, cookie.getName(), cookie.getValue()))
                 .collect(Collectors.joining(DelimiterConstant.SEMICOLON));
-        ResultUtil.addVariableMetaData(COOKIES, webVar.getName(), NO_EXPRESSION, valueResult, result);
+        ResultUtil.addVariableMetaData(COOKIES, var.getName(), NO_EXPRESSION, valueResult, result);
         return valueResult;
     }
 
-    private String getUrlResult(final WebVar webVar, final CommandResult result) {
+    private String getUrlResult(final WebVar var, final CommandResult result) {
         String valueResult = dependencies.getDriver().getCurrentUrl();
-        ResultUtil.addVariableMetaData(URL, webVar.getName(), NO_EXPRESSION, valueResult, result);
+        ResultUtil.addVariableMetaData(URL, var.getName(), NO_EXPRESSION, valueResult, result);
         return valueResult;
     }
 
-    private String getPathResult(final WebVar webVar, final CommandResult commandResult) {
-        return variableService.getPathResult(webVar.getPath(), webVar.getName(),
-                commandResult, dependencies.getScenarioContext());
+    private String getPathResult(final WebVar var, final CommandResult result) {
+        return variableHelper.getPathResult(var.getPath(), var.getName(), dependencies.getScenarioContext(), result);
     }
 
-    private String getExpressionResult(final WebVar webVar, final CommandResult commandResult) {
-        return variableService.getExpressionResult(webVar.getExpression(),
-                webVar.getName(), commandResult, dependencies.getScenarioContext());
+    private String getExpressionResult(final WebVar var, final CommandResult result) {
+        return variableHelper.getExpressionResult(
+                var.getExpression(), var.getName(), dependencies.getScenarioContext(), result);
     }
 
-    private String getFileResult(final WebVar webVar, final CommandResult commandResult) {
-        return variableService.getFileResult(webVar.getFile(),
-                dependencies.getFile(), webVar.getName(), commandResult);
+    private String getFileResult(final WebVar var, final CommandResult result) {
+        return variableHelper.getFileResult(var.getFile(), dependencies.getFile(), var.getName(), result);
     }
 
-    private String getSQLResult(final WebVar webVar, final CommandResult commandResult) {
-        return variableService.getSQLResult(webVar.getSql(),
-                webVar.getName(), commandResult, dependencies.getScenarioContext());
+    private String getSQLResult(final WebVar var, final CommandResult result) {
+        return variableHelper.getSQLResult(var.getSql(), var.getName(), dependencies.getScenarioContext(), result);
     }
-
-    private interface WebVarFromPredicate extends Predicate<WebVar> { }
-
-    private interface WebVarFromMethod extends BiFunction<WebVar, CommandResult, String> { }
 }

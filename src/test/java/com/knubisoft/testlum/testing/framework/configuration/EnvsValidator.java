d22b681888a965229c3065ffd26fe9babba54d9e
Yakymenko Anton <a.yakymenko@knubisoft.com>
2023-05-21 22:09:33 +0300

    refactor

--- src/test/java/com/knubisoft/testlum/testing/framework/configuration/EnvsValidator.java
+++ src/test/java/com/knubisoft/testlum/testing/framework/configuration/EnvsValidator.java
@@ -3,6 +3,7 @@ package com.knubisoft.testlum.testing.framework.configuration;
 import com.knubisoft.testlum.testing.framework.constant.ExceptionMessage;
 import com.knubisoft.testlum.testing.framework.exception.DefaultFrameworkException;
 import com.knubisoft.testlum.testing.framework.util.FileSearcher;
+import com.knubisoft.testlum.testing.model.global_config.Environment;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
@@ -10,17 +11,16 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import static com.knubisoft.testlum.testing.framework.configuration.GlobalTestConfigurationProvider.ENVIRONMENTS;
-
 @Slf4j
 public class EnvsValidator {
-    public void validate(final String configFileName) {
-        List<Optional<File>> configs = collectEnvsConfigsFilesFor(configFileName);
+    public void validate(final String configFileName, final List<Environment> enabledEnvironments) {
+        List<Optional<File>> configs = collectEnvsConfigsFilesFor(configFileName, enabledEnvironments);
         validateEnvsFileSetFor(configFileName, configs);
     }
 
-    private List<Optional<File>> collectEnvsConfigsFilesFor(final String configFileName) {
-        return ENVIRONMENTS.stream()
+    private List<Optional<File>> collectEnvsConfigsFilesFor(final String configFileName,
+                                                            final List<Environment> enabledEnvironments) {
+        return enabledEnvironments.stream()
                 .map(env -> FileSearcher.searchFileFromEnvFolder(env.getFolder(), configFileName))
                 .collect(Collectors.toList());
     }
--- src/test/java/com/knubisoft/testlum/testing/framework/configuration/GlobalTestConfigurationProvider.java
+++ src/test/java/com/knubisoft/testlum/testing/framework/configuration/GlobalTestConfigurationProvider.java
@@ -26,8 +26,8 @@ import java.util.stream.Collectors;
 @Slf4j
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class GlobalTestConfigurationProvider {
-    static final List<Environment> ENVIRONMENTS = filterEnabledEnvironments();
     private static final GlobalTestConfiguration GLOBAL_TEST_CONFIGURATION = init();
+    private static final List<Environment> ENVIRONMENTS = filterEnabledEnvironments();
     private static final EnvsValidator ENVS_VALIDATOR = new EnvsValidator();
     private static final IntegrationsValidator INTEGRATIONS_VALIDATOR = new IntegrationsValidator();
     private static final UiConfigValidator UI_CONFIG_VALIDATOR = new UiConfigValidator();
@@ -83,7 +83,7 @@ public class GlobalTestConfigurationProvider {
     }
 
     private static Map<String, Integrations> collectIntegrations() {
-        ENVS_VALIDATOR.validate(TestResourceSettings.INTEGRATION_CONFIG_FILENAME);
+        ENVS_VALIDATOR.validate(TestResourceSettings.INTEGRATION_CONFIG_FILENAME, getEnabledEnvironments());
         return getEnabledEnvironments().stream()
                 .collect(Collectors.toMap(Environment::getFolder, GlobalTestConfigurationProvider::initIntegration));
     }
@@ -98,7 +98,7 @@ public class GlobalTestConfigurationProvider {
     }
 
     private static Map<String, UiConfig> collectUiConfigs() {
-        ENVS_VALIDATOR.validate(TestResourceSettings.UI_CONFIG_FILENAME);
+        ENVS_VALIDATOR.validate(TestResourceSettings.UI_CONFIG_FILENAME, getEnabledEnvironments());
         return getEnabledEnvironments().stream()
                 .collect(Collectors.toMap(Environment::getFolder, GlobalTestConfigurationProvider::initUiConfig));
     }

94099bb8a249bcdd21f20d9f225eee9fbb85b76c
Alex Cherepashkin <a.cherepashkin@knubisoft.com>
2023-05-28 21:03:17 +0300

    Some changes

--- src/test/java/com/knubisoft/testlum/testing/framework/util/InjectionUtil.java
+++ src/test/java/com/knubisoft/testlum/testing/framework/util/InjectionUtil.java
@@ -1,6 +1,8 @@
 package com.knubisoft.testlum.testing.framework.util;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.knubisoft.testlum.testing.framework.scenario.ScenarioContext;
 import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
@@ -11,12 +13,15 @@ public class InjectionUtil {
     @SneakyThrows
 //    @SuppressWarnings("unchecked")
     public <T> T injectObject(final T t, final ScenarioContext scenarioContext) {
-        String asJson = JacksonMapperUtil.writeValueAsString(t);
+        String asJson = JacksonMapperUtil.instance().writeValueAsString(t);
         String injected = scenarioContext.inject(asJson);
+        JsonNode jsonNode = JacksonMapperUtil.instance().readTree(injected);
 //        Class<T> aClass = (Class<T>) t.getClass();
+//        JavaType javaType = JacksonMapperUtil.instance().getTypeFactory().constructParametricType(aClass, aClass);
         JavaType javaType = JacksonMapperUtil.instance().getTypeFactory().constructType(t.getClass());
 //        return JacksonMapperUtil.readValue(injected, (Class<T>) t.getClass());
-        return JacksonMapperUtil.instance().readValue(injected, javaType);
+//        return JacksonMapperUtil.instance().readValue(injected, javaType);
+        return JacksonMapperUtil.instance().convertValue(jsonNode, javaType);
     }
 
 }

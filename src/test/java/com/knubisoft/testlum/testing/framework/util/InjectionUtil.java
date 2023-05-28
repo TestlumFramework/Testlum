ee8290fcdf4ccfe798d18a51677e443aedeef709
Alex Cherepashkin <a.cherepashkin@knubisoft.com>
2023-05-29 01:02:14 +0300

    Some changes

--- src/test/java/com/knubisoft/testlum/testing/framework/util/InjectionUtil.java
+++ src/test/java/com/knubisoft/testlum/testing/framework/util/InjectionUtil.java
@@ -1,7 +1,5 @@
 package com.knubisoft.testlum.testing.framework.util;
 
-import com.fasterxml.jackson.databind.JavaType;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.knubisoft.testlum.testing.framework.scenario.ScenarioContext;
 import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
@@ -14,12 +12,10 @@ public class InjectionUtil {
     public <T> T injectObject(final T t, final ScenarioContext scenarioContext) {
         String asJson = JacksonMapperUtil.instance().writeValueAsString(t);
         String injected = scenarioContext.inject(asJson);
-        JsonNode jsonNode = JacksonMapperUtil.instance().readTree(injected);
-        Class<T> aClass = (Class<T>) t.getClass();
-        JavaType javaType = JacksonMapperUtil.instance().getTypeFactory().constructType(aClass);
-//        return JacksonMapperUtil.readValue(injected, (Class<T>) t.getClass());
-//        return JacksonMapperUtil.instance().readValue(injected, javaType);
-        return JacksonMapperUtil.instance().convertValue(jsonNode, javaType);
+//            JsonNode jsonNode = JacksonMapperUtil.instance().readTree(injected);
+        T o = JacksonMapperUtil.instance().readerFor(t.getClass()).readValue(injected);
+//        T o = JacksonMapperUtil.instance().readerFor(t.getClass()).createParser(injected).readValueAs((Class<T>)t.getClass());
+        return o;
     }
 
 }

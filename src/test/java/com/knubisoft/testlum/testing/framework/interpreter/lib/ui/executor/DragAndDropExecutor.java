6b4cc17a357a06c40b7eb333637c93e5f72c0da6
Alex Cherepashkin <a.cherepashkin@knubisoft.com>
2023-06-16 14:04:22 +0300

    Fixes/Changes

--- src/test/java/com/knubisoft/testlum/testing/framework/interpreter/lib/ui/executor/DragAndDropExecutor.java
+++ src/test/java/com/knubisoft/testlum/testing/framework/interpreter/lib/ui/executor/DragAndDropExecutor.java
@@ -19,6 +19,8 @@ import org.openqa.selenium.interactions.Actions;
 
 import java.io.File;
 import java.net.URI;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import static com.knubisoft.testlum.testing.framework.constant.ExceptionMessage.DRAG_AND_DROP_FILE_NOT_FOUND;
 import static com.knubisoft.testlum.testing.framework.constant.JavascriptConstant.QUERY_FOR_DRAG_AND_DROP;
@@ -60,7 +62,10 @@ public class DragAndDropExecutor extends AbstractUiExecutor<DragAndDrop> {
         if (!source.exists() || !source.isFile()) {
             throw new DefaultFrameworkException(DRAG_AND_DROP_FILE_NOT_FOUND, source.getName());
         }
-        WebElement input = (WebElement) JavascriptUtil.executeJsScript(QUERY_FOR_DRAG_AND_DROP, driver, target);
-        input.sendKeys(URI.create(source.getCanonicalPath()).getPath());
+        String JS_DROP_FILE = "for(var b=arguments[0],k=arguments[1],l=arguments[2],c=b.ownerDocument,m=0;;){var e=b.getBoundingClientRect(),g=e.left+(k||e.width/2),h=e.top+(l||e.height/2),f=c.elementFromPoint(g,h);if(f&&b.contains(f))break;if(1<++m)throw b=Error('Element not interractable'),b.code=15,b;b.scrollIntoView({behavior:'instant',block:'center',inline:'center'})}var a=c.createElement('INPUT');a.setAttribute('type','file');a.setAttribute('style','position:fixed;z-index:2147483647;left:0;top:0;');a.onchange=function(){var b={effectAllowed:'all',dropEffect:'none',types:['Files'],files:this.files,setData:function(){},getData:function(){},clearData:function(){},setDragImage:function(){}};window.DataTransferItemList&&(b.items=Object.setPrototypeOf([Object.setPrototypeOf({kind:'file',type:this.files[0].type,file:this.files[0],getAsFile:function(){return this.file},getAsString:function(b){var a=new FileReader;a.onload=function(a){b(a.target.result)};a.readAsText(this.file)}},DataTransferItem.prototype)],DataTransferItemList.prototype));Object.setPrototypeOf(b,DataTransfer.prototype);['dragenter','dragover','drop'].forEach(function(a){var d=c.createEvent('DragEvent');d.initMouseEvent(a,!0,!0,c.defaultView,0,0,0,g,h,!1,!1,!1,!1,0,null);Object.setPrototypeOf(d,null);d.dataTransfer=b;Object.setPrototypeOf(d,DragEvent.prototype);f.dispatchEvent(d)});a.parentElement.removeChild(a)};c.documentElement.appendChild(a);a.getBoundingClientRect();return a;";
+        WebElement input = (WebElement) JavascriptUtil.executeJsScript(JS_DROP_FILE, driver, target, 0, 0);
+        Path currentRelativePath = Paths.get(source.getAbsolutePath());
+        String path = currentRelativePath.toAbsolutePath().toString();
+        input.sendKeys(path);
     }
 }

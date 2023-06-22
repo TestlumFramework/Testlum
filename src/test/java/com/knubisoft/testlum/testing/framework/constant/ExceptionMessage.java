c1b0a31389bb3f3b7261e173801b70801f9f094a
Shumskyi Mykyta <n.shumsky@knubisoft.com>
2023-06-22 15:10:27 +0300

    - refactored exception message

--- src/test/java/com/knubisoft/testlum/testing/framework/constant/ExceptionMessage.java
+++ src/test/java/com/knubisoft/testlum/testing/framework/constant/ExceptionMessage.java
@@ -227,8 +227,7 @@ public class ExceptionMessage {
     public static final String TAB_NOT_FOUND = "Available tab that can be closed not found";
     public static final String TAB_OUT_OF_BOUNDS = "Cannot close the tab №<%s> because there are only %s tabs";
     public static final String CANNOT_SWITCH_TO_WEBVIEW = "Cannot switch to the web view. Web context was not found";
-    public static final String FAILED_CONDITION_LOG = ANSI_RED
-            + "Condition '%s' is not a valid boolean expression\n"
-            + "Converted value: %s\n"
+    public static final String FAILED_CONDITION_LOG = ANSI_RED + "Condition '%s' is not a valid boolean expression\n"
+            + "Converted condition: %s\n"
             + "Parsing exception: %s" + ANSI_RESET;
 }

e2f6e2f454f6be5100fd4d150c91f596de3cace3
Shumskyi Mykyta <n.shumsky@knubisoft.com>
2023-08-28 17:56:51 +0300

    - refactored

--- src/test/java/com/knubisoft/testlum/runner/impl/TestRunner.java
+++ src/test/java/com/knubisoft/testlum/runner/impl/TestRunner.java
@@ -3,12 +3,10 @@ package com.knubisoft.testlum.runner.impl;
 import com.knubisoft.testlum.runner.Runner;
 import com.knubisoft.testlum.testing.RootTest;
 import com.knubisoft.testlum.testing.framework.SystemInfo;
-import com.knubisoft.testlum.testing.framework.configuration.GlobalTestConfigurationProvider;
 import com.knubisoft.testlum.testing.framework.configuration.TestResourceSettings;
 import com.knubisoft.testlum.testing.framework.util.ArgumentsUtils;
 import com.knubisoft.testlum.testing.framework.util.LogUtil;
 import com.knubisoft.testlum.testing.framework.util.ResultUtil;
-import lombok.SneakyThrows;
 import org.junit.platform.launcher.Launcher;
 import org.junit.platform.launcher.LauncherDiscoveryRequest;
 import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
@@ -16,27 +14,23 @@ import org.junit.platform.launcher.core.LauncherFactory;
 import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
 import org.junit.platform.launcher.listeners.TestExecutionSummary;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.nio.file.Files;
-import java.util.Properties;
-
+import static com.knubisoft.testlum.testing.framework.configuration.GlobalTestConfigurationProvider.provide;
 import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
 
 public class TestRunner implements Runner {
 
-    private static final String JUNIT_PROPERTIES_FILENAME = "junit-platform.properties";
-    private static final String PARALLEL_ENABLED = "junit.jupiter.execution.parallel.enabled=true";
-    private static final String PARALLEL_DISABLED = "junit.jupiter.execution.parallel.enabled=false";
+    private static final String PARALLEL = "junit.jupiter.execution.parallel.enabled";
+    private static final String STRATEGY = "junit.jupiter.execution.parallel.config.strategy";
+    private static final String CLASS = "junit.jupiter.execution.parallel.config.custom.class";
+    private static final String JUNIT_STRATEGY_CUSTOM = "custom";
+    private static final String JUNIT_PARALLEL_CONFIG =
+            "com.knubisoft.testlum.testing.framework.env.parallel.GlobalParallelExecutionConfigStrategy";
 
     @Override
     public void run(final String[] args) {
         String configFileName = ArgumentsUtils.getConfigurationFileName(args[0]);
         String pathToTestResources = ArgumentsUtils.getPathToTestResources(args[1]);
         TestResourceSettings.init(configFileName, pathToTestResources);
-        isParallelExecution();
         TestExecutionSummary testExecutionSummary = runTests();
         LogUtil.logTestExecutionSummary(testExecutionSummary);
         if (SystemInfo.TESTING_IN_PIPELINE) {
@@ -44,26 +38,13 @@ public class TestRunner implements Runner {
         }
     }
 
-    @SneakyThrows
-    private void isParallelExecution() {
-        if (!GlobalTestConfigurationProvider.provide().isParallelExecution()) {
-            Properties properties = new Properties();
-            try (InputStream inputStream = ClassLoader.getSystemResourceAsStream(JUNIT_PROPERTIES_FILENAME);
-                 OutputStream outputStream = Files.newOutputStream(
-                         new File(ClassLoader.getSystemResource(JUNIT_PROPERTIES_FILENAME).toURI()).toPath())) {
-                properties.load(inputStream);
-                properties.setProperty(PARALLEL_ENABLED, PARALLEL_DISABLED);
-                properties.store(outputStream, null);
-            } catch (IOException e) {
-                //ignore
-            }
-        }
-    }
-
     private TestExecutionSummary runTests() {
         LauncherDiscoveryRequest tests = LauncherDiscoveryRequestBuilder
                 .request()
                 .selectors(selectClass(RootTest.class))
+                .configurationParameter(PARALLEL, provide().isParallelExecution().toString())
+                .configurationParameter(STRATEGY, JUNIT_STRATEGY_CUSTOM)
+                .configurationParameter(CLASS, JUNIT_PARALLEL_CONFIG)
                 .build();
         Launcher launcher = LauncherFactory.create();
         SummaryGeneratingListener listener = new SummaryGeneratingListener();

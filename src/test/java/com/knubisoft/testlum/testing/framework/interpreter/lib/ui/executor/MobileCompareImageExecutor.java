9214b1f85ce79fd3f733d7fb2e68e043779dff3b
Shumskyi Mykyta <n.shumsky@knubisoft.com>
2023-09-07 17:30:21 +0300

    - added exception for iOs mobilebrowser devices for part command

--- src/test/java/com/knubisoft/testlum/testing/framework/interpreter/lib/ui/executor/MobileCompareImageExecutor.java
+++ src/test/java/com/knubisoft/testlum/testing/framework/interpreter/lib/ui/executor/MobileCompareImageExecutor.java
@@ -1,9 +1,11 @@
 package com.knubisoft.testlum.testing.framework.interpreter.lib.ui.executor;
 
 import com.github.romankh3.image.comparison.model.ImageComparisonResult;
+import com.knubisoft.testlum.testing.framework.exception.DefaultFrameworkException;
 import com.knubisoft.testlum.testing.framework.interpreter.lib.ui.AbstractUiExecutor;
 import com.knubisoft.testlum.testing.framework.interpreter.lib.ui.ExecutorDependencies;
 import com.knubisoft.testlum.testing.framework.interpreter.lib.ui.ExecutorForClass;
+import com.knubisoft.testlum.testing.framework.interpreter.lib.ui.UiType;
 import com.knubisoft.testlum.testing.framework.report.CommandResult;
 import com.knubisoft.testlum.testing.framework.util.FileSearcher;
 import com.knubisoft.testlum.testing.framework.util.ImageComparator;
@@ -26,6 +28,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 
+import static com.knubisoft.testlum.testing.framework.constant.ExceptionMessage.IOS_NOT_SUPPORT_PART_COMMAND;
 import static com.knubisoft.testlum.testing.framework.constant.LogMessage.URL_TO_IMAGE_LOG;
 import static com.knubisoft.testlum.testing.framework.util.ResultUtil.URL_TO_ACTUAL_IMAGE;
 import static java.util.Objects.nonNull;
@@ -60,6 +63,9 @@ public class MobileCompareImageExecutor extends AbstractUiExecutor<MobileImage> 
             return extractImageFromElement(webElement, image.getPicture().getAttribute(), result);
         }
         if (nonNull(image.getPart())) {
+            if (UiType.MOBILE_BROWSER.equals(dependencies.getUiType()) && isIosDevice(webDriver)){
+                throw new DefaultFrameworkException(IOS_NOT_SUPPORT_PART_COMMAND);
+            }
             WebElement webElement = UiUtil.findWebElement(dependencies, image.getPart().getLocatorId());
             return ImageIO.read(UiUtil.takeScreenshot(webElement));
         }
@@ -79,12 +85,16 @@ public class MobileCompareImageExecutor extends AbstractUiExecutor<MobileImage> 
     private BufferedImage cutStatusBar(final FullScreen fullScreen,
                                        final BufferedImage screenshot,
                                        final WebDriver driver) {
-        Platform platformName = ((RemoteWebDriver) driver).getCapabilities().getPlatformName();
-        if (nonNull(fullScreen) && platformName.equals(Platform.MAC)) {
+        if (nonNull(fullScreen) && isIosDevice(driver)) {
             int statusBarHeight = ImageComparisonUtil.getStatusBarHeight(driver);
             return screenshot.getSubimage(0, statusBarHeight, screenshot.getWidth(),
                     screenshot.getHeight() - statusBarHeight);
         }
         return screenshot;
+    }
+
+    private boolean isIosDevice(final WebDriver driver) {
+        Platform platformName = ((RemoteWebDriver) driver).getCapabilities().getPlatformName();
+        return platformName.equals(Platform.MAC);
     }
 }

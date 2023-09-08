7bfa3d75cda41fcf783ca8c5090258bfb1853ba1
Shumskyi Mykyta <n.shumsky@knubisoft.com>
2023-09-08 12:41:02 +0300

    - added method to retrieve native ios statusBarHeight

--- src/test/java/com/knubisoft/testlum/testing/framework/interpreter/lib/ui/executor/NativeCompareImageExecutor.java
+++ src/test/java/com/knubisoft/testlum/testing/framework/interpreter/lib/ui/executor/NativeCompareImageExecutor.java
@@ -15,20 +15,28 @@ import com.knubisoft.testlum.testing.model.scenario.FullScreen;
 import com.knubisoft.testlum.testing.model.scenario.NativeImage;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static java.util.Objects.nonNull;
 
 @Slf4j
 @ExecutorForClass(NativeImage.class)
 public class NativeCompareImageExecutor extends AbstractUiExecutor<NativeImage> {
+
+    private static final String IOS_NAVIGATION_BAR = "XCUIElementTypeNavigationBar";
+    private static final String Y_MIN_POINT = "y=\"(\\d+)\"";
 
     public NativeCompareImageExecutor(final ExecutorDependencies dependencies) {
         super(dependencies);
@@ -42,8 +50,7 @@ public class NativeCompareImageExecutor extends AbstractUiExecutor<NativeImage> 
         File scenarioFile = dependencies.getFile();
         BufferedImage expected = ImageIO.read(FileSearcher.searchFileFromDir(scenarioFile, image.getFile()));
         BufferedImage actual = getActualImage(dependencies.getDriver(), image);
-        ImageComparisonResult comparisonResult = ImageComparator.compare(image, expected,
-                cutStatusBar(image.getFullScreen(), actual, dependencies.getDriver()));
+        ImageComparisonResult comparisonResult = ImageComparator.compare(image, expected, actual);
         ImageComparisonUtil.processImageComparisonResult(comparisonResult, image.getFile(),
                 image.isHighlightDifference(), scenarioFile.getParentFile(), result);
     }
@@ -54,17 +61,45 @@ public class NativeCompareImageExecutor extends AbstractUiExecutor<NativeImage> 
             WebElement webElement = UiUtil.findWebElement(dependencies, image.getPart().getLocatorId());
             return ImageIO.read(webElement.getScreenshotAs(OutputType.FILE));
         }
-        return ImageIO.read(UiUtil.takeScreenshot(webDriver));
+        BufferedImage fullScreen = ImageIO.read(UiUtil.takeScreenshot(webDriver));
+        return cutStatusBar(image.getFullScreen(), fullScreen, webDriver);
     }
 
     public BufferedImage cutStatusBar(final FullScreen fullScreen,
                                       final BufferedImage screenshot,
                                       final WebDriver driver) {
         if (nonNull(fullScreen)) {
-            int statusBarHeight = ImageComparisonUtil.getStatusBarHeight(driver);
+            int statusBarHeight = getStatusBarHeight(screenshot, driver);
             return screenshot.getSubimage(0, statusBarHeight, screenshot.getWidth(),
                     screenshot.getHeight() - statusBarHeight);
         }
         return screenshot;
+    }
+
+    private int getStatusBarHeight(final BufferedImage screenshot, final WebDriver driver) {
+        Platform platformName = ((RemoteWebDriver) driver).getCapabilities().getPlatformName();
+        return platformName.equals(Platform.MAC) || platformName.equals(Platform.IOS)
+                ? getIosStatusBarHeight(screenshot, driver) : ImageComparisonUtil.getStatusBarHeight(driver);
+    }
+
+    private int getIosStatusBarHeight(final BufferedImage screenshot, final WebDriver driver) {
+        String s = driver.getPageSource().lines()
+                .filter(line -> line.contains(IOS_NAVIGATION_BAR))
+                .findFirst().orElse(StringUtils.EMPTY);
+        Pattern r = Pattern.compile(Y_MIN_POINT);
+        Matcher m = r.matcher(s);
+        if (m.find()) {
+            String yValueStr = m.group(1);
+            return scaleStatusBar(Integer.parseInt(yValueStr), screenshot, driver);
+        }
+        return 0;
+    }
+
+    private int scaleStatusBar(final int statusBarHeight,
+                               final BufferedImage screenshot,
+                               final WebDriver driver) {
+        int screenHeight = driver.manage().window().getSize().getHeight();
+        int scaling = screenshot.getHeight() > screenHeight ? screenshot.getHeight() / screenHeight : 1;
+        return statusBarHeight * scaling;
     }
 }

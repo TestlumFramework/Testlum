f85efe64457eece59a283794ca9ea10c4ffcbac2
borislavmarusov <b.marusov@knubisoft.com>
2023-11-22 17:27:43 +0200

    Fixed auto healing logic

--- engine/src/test/java/com/knubisoft/testlum/testing/framework/util/WebElementFinder.java
+++ engine/src/test/java/com/knubisoft/testlum/testing/framework/util/WebElementFinder.java
@@ -1,6 +1,5 @@
 package com.knubisoft.testlum.testing.framework.util;
 
-import com.knubisoft.testlum.testing.framework.constant.ExceptionMessage;
 import com.knubisoft.testlum.testing.framework.exception.DefaultFrameworkException;
 import com.knubisoft.testlum.testing.model.pages.ClassName;
 import com.knubisoft.testlum.testing.model.pages.CssSelector;
@@ -16,44 +15,42 @@ import org.openqa.selenium.WebElement;
 
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
-
-import static java.util.Objects.nonNull;
 
 @Slf4j
 @UtilityClass
 public final class WebElementFinder {
 
-    private static final Map<LocatorType, ByType> SEARCH_TYPES;
+    private static final Map<Class<?>, ByType> SEARCH_TYPES;
 
     static {
-        final Map<LocatorType, ByType> map = new HashMap<>();
-        map.put(l -> nonNull(getLocatorsByType(l, Xpath.class)), l -> By.xpath(getLocatorsByType(l, Xpath.class)));
-        map.put(l -> nonNull(getLocatorsByType(l, Id.class)), l -> By.id(getLocatorsByType(l, Id.class)));
-        map.put(l -> nonNull(getLocatorsByType(l, ClassName.class)), l -> By.className(getLocatorsByType(l, ClassName.class)));
-        map.put(l -> nonNull(getLocatorsByType(l, CssSelector.class)), l -> By.cssSelector(getLocatorsByType(l, CssSelector.class)));
-        map.put(l -> nonNull(getLocatorsByType(l, Text.class)), l -> By.text(getLocatorsByType(l, Text.class)));
+        final Map<Class<?>, ByType> map = new HashMap<>();
+        map.put(Xpath.class, l -> By.xpath(getLocatorsByType(l, Xpath.class)));
+        map.put(Id.class, l -> By.id(getLocatorsByType(l, Id.class)));
+        map.put(ClassName.class, l -> By.className(getLocatorsByType(l, ClassName.class)));
+        map.put(CssSelector.class, l -> By.cssSelector(getLocatorsByType(l, CssSelector.class)));
+        map.put(Text.class, l -> By.text(getLocatorsByType(l, Text.class)));
         SEARCH_TYPES = Collections.unmodifiableMap(map);
     }
 
     public WebElement find(final Locator locator, final WebDriver driver) {
-        return SEARCH_TYPES.entrySet().stream()
-                .filter(l -> l.getKey().test(locator))
-                .findFirst()
-                .map(l -> l.getValue().apply(locator))
-                .map(byList -> getElementFromLocatorList(byList, driver))
-                .orElseThrow(() -> new DefaultFrameworkException(ExceptionMessage.WEB_ELEMENT_BY_LOCATOR_NOT_FOUND,
-                        locator.getLocatorId()));
+        Set<org.openqa.selenium.By> bySet = new HashSet<>();
+        locator.getXpathOrIdOrClassName().forEach(obj -> {
+            Class<?> clazz = obj.getClass();
+            bySet.addAll(SEARCH_TYPES.get(clazz).apply(locator));
+        });
+        return getElementFromLocatorList(bySet, driver);
     }
 
-    public WebElement getElementFromLocatorList(final List<org.openqa.selenium.By> byList, final WebDriver driver) {
+    public WebElement getElementFromLocatorList(final Set<org.openqa.selenium.By> bySet, final WebDriver driver) {
         boolean anyLocatorSucceeded = false;
         WebElement element = null;
-        for (org.openqa.selenium.By by : byList) {
+        for (org.openqa.selenium.By by : bySet) {
             try {
                 element = driver.findElement(by);
                 anyLocatorSucceeded = true;
@@ -70,8 +67,8 @@ public final class WebElementFinder {
         Map<Class<?>, List<Object>> locatorTypeMap = splitByLocatorType(locator);
         return (List<T>) locatorTypeMap.entrySet().stream()
                 .filter(e -> e.getKey().equals(clazz))
-                .map(Map.Entry::getValue)
                 .findFirst()
+                .map(Map.Entry::getValue)
                 .orElseThrow(() -> new DefaultFrameworkException("There is no locator type like this"));
     }
 
@@ -81,7 +78,5 @@ public final class WebElementFinder {
                 .collect(Collectors.groupingBy(Object::getClass));
     }
 
-
-    private interface LocatorType extends Predicate<Locator> { }
     private interface ByType extends Function<Locator, List<org.openqa.selenium.By>> { }
 }

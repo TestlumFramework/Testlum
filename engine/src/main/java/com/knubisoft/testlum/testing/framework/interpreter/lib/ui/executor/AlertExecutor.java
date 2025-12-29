141ccf61fdc9af4bc0e29bb7eca4a1671944241b
Mykhailo Rudyk <m.rudyk@testlum.com>
2025-12-29 14:20:04 +0200

    basic alert logic impl

---
+++ engine/src/main/java/com/knubisoft/testlum/testing/framework/interpreter/lib/ui/executor/AlertExecutor.java
@@ -0,0 +1,55 @@
+package com.knubisoft.testlum.testing.framework.interpreter.lib.ui.executor;
+
+import com.knubisoft.testlum.testing.framework.interpreter.lib.ui.AbstractUiExecutor;
+import com.knubisoft.testlum.testing.framework.interpreter.lib.ui.ExecutorDependencies;
+import com.knubisoft.testlum.testing.framework.interpreter.lib.ui.ExecutorForClass;
+import com.knubisoft.testlum.testing.framework.report.CommandResult;
+import com.knubisoft.testlum.testing.framework.util.UiUtil;
+import com.knubisoft.testlum.testing.model.scenario.Alert;
+import com.knubisoft.testlum.testing.model.scenario.AlertAction;
+import com.knubisoft.testlum.testing.model.scenario.AlertType;
+
+import static com.knubisoft.testlum.testing.framework.util.ResultUtil.ALERT_TYPE;
+
+@ExecutorForClass(Alert.class)
+public class AlertExecutor extends AbstractUiExecutor<Alert> {
+
+    protected AlertExecutor(ExecutorDependencies dependencies) {
+        super(dependencies);
+    }
+
+    @Override
+    protected void execute(Alert alert, CommandResult result) {
+        result.put(ALERT_TYPE, alert.getType().value());
+        uiUtil.takeScreenshotAndSaveIfRequired(result, dependencies);
+        processAlertCommand(alert, result);
+    }
+
+    private void processAlertCommand(Alert alert, CommandResult result) {
+        if (AlertType.PROMPT == alert.getType() && alert.getText() != null) {
+           setPromptText(alert.getText());
+        }
+
+        if (AlertAction.ACCEPT == alert.getAction()) {
+            acceptAlert();
+        } else {
+            dismissAlert();
+        }
+    }
+
+    private void acceptAlert() {
+        dependencies.getDriver().switchTo().alert().accept();
+    }
+
+    private void dismissAlert() {
+        dependencies.getDriver().switchTo().alert().dismiss();
+    }
+
+    private String getAlertText(Object alert) {
+        return dependencies.getDriver().switchTo().alert().getText();
+    }
+
+    private void setPromptText(String alertText) {
+        dependencies.getDriver().switchTo().alert().sendKeys(alertText);
+    }
+}

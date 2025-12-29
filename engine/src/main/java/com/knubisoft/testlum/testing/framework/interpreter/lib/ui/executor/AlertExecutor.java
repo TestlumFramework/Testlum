48fcf15a29a5cb1cdf88f4d62d1b4206aa6a512f
Mykhailo Rudyk <m.rudyk@testlum.com>
2025-12-29 14:22:30 +0200

    alert executor fix

--- engine/src/main/java/com/knubisoft/testlum/testing/framework/interpreter/lib/ui/executor/AlertExecutor.java
+++ engine/src/main/java/com/knubisoft/testlum/testing/framework/interpreter/lib/ui/executor/AlertExecutor.java
@@ -14,12 +14,12 @@ import static com.knubisoft.testlum.testing.framework.util.ResultUtil.ALERT_TYPE
 @ExecutorForClass(Alert.class)
 public class AlertExecutor extends AbstractUiExecutor<Alert> {
 
-    protected AlertExecutor(ExecutorDependencies dependencies) {
+    public AlertExecutor(ExecutorDependencies dependencies) {
         super(dependencies);
     }
 
     @Override
-    protected void execute(Alert alert, CommandResult result) {
+    public void execute(Alert alert, CommandResult result) {
         result.put(ALERT_TYPE, alert.getType().value());
         uiUtil.takeScreenshotAndSaveIfRequired(result, dependencies);
         processAlertCommand(alert, result);

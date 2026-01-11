671f9ce4e967aef0ae404dc330d92fe7f7588dcc
MaksymKalennyk <107300165+MaksymKalennyk@users.noreply.github.com>
2026-01-11 14:58:03 +0200

    TBE-2348
    implemented date variable

--- modules/variable-util/src/main/java/com/knubisoft/testlum/testing/framework/variable/util/VariableHelper.java
+++ modules/variable-util/src/main/java/com/knubisoft/testlum/testing/framework/variable/util/VariableHelper.java
@@ -2,7 +2,15 @@ package com.knubisoft.testlum.testing.framework.variable.util;
 
 import com.knubisoft.testlum.testing.framework.report.CommandResult;
 import com.knubisoft.testlum.testing.framework.scenario.ScenarioContext;
-import com.knubisoft.testlum.testing.model.scenario.*;
+import com.knubisoft.testlum.testing.model.scenario.AbstractCommand;
+import com.knubisoft.testlum.testing.model.scenario.FromConstant;
+import com.knubisoft.testlum.testing.model.scenario.FromDate;
+import com.knubisoft.testlum.testing.model.scenario.FromExpression;
+import com.knubisoft.testlum.testing.model.scenario.FromFile;
+import com.knubisoft.testlum.testing.model.scenario.FromPath;
+import com.knubisoft.testlum.testing.model.scenario.FromRandomGenerate;
+import com.knubisoft.testlum.testing.model.scenario.FromSQL;
+import com.knubisoft.testlum.testing.model.scenario.FromAlert;
 import org.openqa.selenium.Alert;
 
 import java.util.Map;
@@ -29,6 +37,8 @@ public interface VariableHelper {
     String getSQLResult(FromSQL fromSQL, String varName, CommandResult result);
 
     String getAlertResult(FromAlert fromAlert, String varName, Alert browserAlert, CommandResult result);
+
+    String getDateResult(FromDate fromDate, String varName, CommandResult result);
 
     interface VarPredicate<T extends AbstractCommand> extends Predicate<T> { }
 

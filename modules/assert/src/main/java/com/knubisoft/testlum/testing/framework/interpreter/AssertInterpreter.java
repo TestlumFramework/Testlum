d5a1c1984aaa871f397106d96a05ad1d83f93b50
Bohdan Shakhov <b.shakhov@testlum.com>
2026-02-02 16:12:33 +0200

    Add condition processing to nested general assert commands

--- modules/assert/src/main/java/com/knubisoft/testlum/testing/framework/interpreter/AssertInterpreter.java
+++ modules/assert/src/main/java/com/knubisoft/testlum/testing/framework/interpreter/AssertInterpreter.java
@@ -5,13 +5,17 @@ import com.knubisoft.testlum.testing.framework.interpreter.lib.AbstractInterpret
 import com.knubisoft.testlum.testing.framework.interpreter.lib.InterpreterDependencies;
 import com.knubisoft.testlum.testing.framework.interpreter.lib.InterpreterForClass;
 import com.knubisoft.testlum.testing.framework.report.CommandResult;
+import com.knubisoft.testlum.testing.framework.scenario.ScenarioContext;
 import com.knubisoft.testlum.testing.model.scenario.AbstractCommand;
 import com.knubisoft.testlum.testing.model.scenario.Assert;
 import com.knubisoft.testlum.testing.model.scenario.AssertEqual;
 import com.knubisoft.testlum.testing.model.scenario.AssertEquality;
 import com.knubisoft.testlum.testing.model.scenario.AssertNotEqual;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.StopWatch;
+import org.springframework.expression.Expression;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -60,7 +64,9 @@ public class AssertInterpreter extends AbstractInterpreter<Assert> {
             subCommandsResult.add(commandResult);
             logAssertEqualityCommand(action, commandId);
             addAssertEqualityMetaData(action, commandResult);
-            processEachAction(action, commandResult);
+            if (conditionProvider.isTrue(action.getCondition(), dependencies.getScenarioContext(), commandResult)) {
+                processEachAction(action, commandResult);
+            }
         });
         setExecutionResultIfSubCommandsFailed(result);
     }

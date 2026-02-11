0b36cbbe1354c6b7e0c51fe6735aa220a157e342
Bohdan Shakhov <b.shakhov@testlum.com>
2026-02-11 10:24:29 +0200

    TBE-2328
    - Remove duplicate locator generation in persistent mode
    
    - Keep correct ordering of locators during healing generation in persistent mode
    
    - Generate patches with healed locators values in xml format instead of json

--- engine/src/main/java/com/knubisoft/testlum/testing/framework/autohealing/dto/HealedLocators.java
+++ engine/src/main/java/com/knubisoft/testlum/testing/framework/autohealing/dto/HealedLocators.java
@@ -1,5 +1,9 @@
 package com.knubisoft.testlum.testing.framework.autohealing.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,12 +15,18 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@JacksonXmlRootElement(localName = "healedLocator")
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class HealedLocators {
 
+    @JacksonXmlElementWrapper(useWrapping = false)
+    @JacksonXmlProperty(localName = "xpath")
     private List<String> xpaths;
+
+    @JacksonXmlElementWrapper(useWrapping = false)
+    @JacksonXmlProperty(localName = "cssSelector")
     private List<String> cssSelectors;
     private String id;
     private String className;
     private String text;
-
 }

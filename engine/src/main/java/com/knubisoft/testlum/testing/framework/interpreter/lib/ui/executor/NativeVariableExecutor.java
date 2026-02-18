83cb7315661c2fb4c2b7238779f25a2a454e6748
MaksymKalennyk <107300165+MaksymKalennyk@users.noreply.github.com>
2026-02-18 12:27:40 +0100

    TBE-2536
    added date to native and web executors

--- engine/src/main/java/com/knubisoft/testlum/testing/framework/interpreter/lib/ui/executor/NativeVariableExecutor.java
+++ engine/src/main/java/com/knubisoft/testlum/testing/framework/interpreter/lib/ui/executor/NativeVariableExecutor.java
@@ -37,6 +37,7 @@ public class NativeVariableExecutor extends AbstractUiExecutor<NativeVar> {
                 var -> nonNull(var.getExpression()), this::getExpressionResult,
                 var -> nonNull(var.getFile()), this::getFileResult,
                 var -> nonNull(var.getSql()), this::getSQLResult,
+                var -> nonNull(var.getDate()), this::getDateResult,
                 var -> nonNull(var.getGenerate()), this::getRandomGenerateResult);
         this.variableHelper = dependencies.getContext().getBean(VariableHelper.class);
     }
@@ -98,6 +99,10 @@ public class NativeVariableExecutor extends AbstractUiExecutor<NativeVar> {
 
     private String getSQLResult(final NativeVar var, final CommandResult result) {
         return variableHelper.getSQLResult(var.getSql(), var.getName(), result);
+    }
+
+    private String getDateResult(final NativeVar var, final CommandResult result) {
+        return variableHelper.getDateResult(var.getDate(), var.getName(), result);
     }
 
     private String getRandomGenerateResult(final NativeVar var, final CommandResult result) {

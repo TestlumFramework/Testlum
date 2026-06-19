655b5462c70b17d77a23b4c5dc4add2eeef1dd82
MaksymKalennyk <107300165+MaksymKalennyk@users.noreply.github.com>
2026-06-19 10:47:31 +0300

    removed attr comment from otp tag

--- modules/models/src/main/java/com/knubisoft/testlum/testing/model/scenario/FromOtp.java
+++ modules/models/src/main/java/com/knubisoft/testlum/testing/model/scenario/FromOtp.java
@@ -17,7 +17,6 @@ import jakarta.xml.bind.annotation.XmlType;
  *   &lt;complexContent&gt;
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
  *       &lt;attribute name="secretKey" use="required" type="{http://www.knubisoft.com/testlum/testing/model/scenario}nonEmptyString" /&gt;
- *       &lt;attribute name="comment" type="{http://www.knubisoft.com/testlum/testing/model/scenario}stringMin10" /&gt;
  *     &lt;/restriction&gt;
  *   &lt;/complexContent&gt;
  * &lt;/complexType&gt;
@@ -31,8 +30,6 @@ public class FromOtp {
 
     @XmlAttribute(name = "secretKey", required = true)
     protected String secretKey;
-    @XmlAttribute(name = "comment")
-    protected String comment;
 
     /**
      * Gets the value of the secretKey property.
@@ -57,29 +54,4 @@ public class FromOtp {
     public void setSecretKey(String value) {
         this.secretKey = value;
     }
-
-    /**
-     * Gets the value of the comment property.
-     * 
-     * @return
-     *     possible object is
-     *     {@link String }
-     *     
-     */
-    public String getComment() {
-        return comment;
-    }
-
-    /**
-     * Sets the value of the comment property.
-     * 
-     * @param value
-     *     allowed object is
-     *     {@link String }
-     *     
-     */
-    public void setComment(String value) {
-        this.comment = value;
-    }
-
 }

280977453e95390d1cef00d92b397e92dd13009a
Bohdan Shakhov <b.shakhov@testlum.com>
2026-08-21 16:36:28 +0300

    TBE-3402
    reformat UI config log table structure

--- common/log/src/main/java/com/knubisoft/testlum/testing/logger/UiConfigurationLogger.java
+++ common/log/src/main/java/com/knubisoft/testlum/testing/logger/UiConfigurationLogger.java
@@ -14,14 +14,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
 public class UiConfigurationLogger {
 
-    public static final int COLUMN_COUNT = 4;
+    public static final int COLUMN_COUNT = 3;
 
     public void appendEnvironmentSections(final DynamicTableBuilder table,
                                           final String environment,
@@ -45,110 +43,64 @@ public class UiConfigurationLogger {
         if (web == null) {
             return;
         }
-        String aliases = joinValues(web.getBrowserSettings().getBrowsers().getChromeOrFirefoxOrSafari(),
-                AbstractBrowser::getAlias);
-        String browserTypes = joinValues(web.getBrowserSettings().getBrowsers().getChromeOrFirefoxOrSafari(),
-                browser -> browser.getClass().getSimpleName());
-        addSectionTitle(table, LogMessage.UI_CONFIG_TABLE_WEB_ROW);
-        addWebHeaders(table);
-        addWebDataRow(table, web, aliases, browserTypes);
+        addSectionHeader(table, LogMessage.UI_CONFIG_TABLE_WEB_ROW, web.isEnabled());
+        table.span(String.format(LogMessage.UI_CONFIG_TABLE_BASE_URL_ROW, web.getBaseUrl()));
+        addBrowserSection(table, web.getBrowserSettings().getBrowsers().getChromeOrFirefoxOrSafari());
+    }
+
+    private void addBrowserSection(final DynamicTableBuilder table,
+                                   final List<? extends AbstractBrowser> browsers) {
+        table.row(LogMessage.UI_CONFIG_TABLE_BROWSER_HEADER,
+                LogMessage.UI_CONFIG_TABLE_ALIAS_HEADER,
+                LogMessage.UI_CONFIG_TABLE_ENABLED_HEADER);
+        for (AbstractBrowser browser : browsers) {
+            table.row(computeRowColor(browser.isEnabled()),
+                    browser.getClass().getSimpleName(),
+                    browser.getAlias(),
+                    browser.isEnabled());
+        }
     }
 
     private void addMobileBrowserConfiguration(final DynamicTableBuilder table, final Mobilebrowser mobilebrowser) {
         if (mobilebrowser == null) {
             return;
         }
-        String aliases = joinValues(mobilebrowser.getDevices().getDevice(), AbstractDevice::getAlias);
-        addSectionTitle(table, LogMessage.UI_CONFIG_TABLE_MOBILE_BROWSER_ROW);
-        addMobileHeaders(table);
-        addMobileDataRow(table, mobilebrowser, aliases);
+        addSectionHeader(table, LogMessage.UI_CONFIG_TABLE_MOBILE_BROWSER_ROW, mobilebrowser.isEnabled());
+        table.span(String.format(LogMessage.UI_CONFIG_TABLE_BASE_URL_ROW, mobilebrowser.getBaseUrl()));
+        addDeviceSection(table, mobilebrowser.getConnection(), mobilebrowser.getDevices().getDevice());
     }
 
     private void addNativeConfiguration(final DynamicTableBuilder table, final Native nativeConfiguration) {
         if (nativeConfiguration == null) {
             return;
         }
-        String aliases = joinValues(nativeConfiguration.getDevices().getDevice(), AbstractDevice::getAlias);
-        String platforms = joinValues(nativeConfiguration.getDevices().getDevice(),
-                abstractDevice -> abstractDevice.getPlatformName().value());
-        addSectionTitle(table, LogMessage.UI_CONFIG_TABLE_NATIVE_ROW);
-        addNativeHeaders(table);
-        addNativeDataRow(table, nativeConfiguration, platforms, aliases);
+        addSectionHeader(table, LogMessage.UI_CONFIG_TABLE_NATIVE_ROW, nativeConfiguration.isEnabled());
+        addDeviceSection(table, nativeConfiguration.getConnection(), nativeConfiguration.getDevices().getDevice());
     }
 
-    private void addWebHeaders(final DynamicTableBuilder table) {
-        table.row(
-                LogMessage.UI_CONFIG_TABLE_BASE_URL_HEADER,
-                LogMessage.UI_CONFIG_TABLE_BROWSER_ALIAS_HEADER,
-                LogMessage.UI_CONFIG_TABLE_BROWSER_TYPE_HEADER,
-                LogMessage.UI_CONFIG_TABLE_ENABLED_HEADER
-        );
-    }
-
-    private void addWebDataRow(final DynamicTableBuilder table,
-                               final Web web,
-                               final String aliases,
-                               final String browserTypes) {
-        table.row(
-                computeRowColor(web.isEnabled()),
-                web.getBaseUrl(),
-                aliases,
-                browserTypes,
-                web.isEnabled()
-        );
-    }
-
-    private void addMobileHeaders(final DynamicTableBuilder table) {
-        table.row(
-                LogMessage.UI_CONFIG_TABLE_BASE_URL_HEADER,
+    private void addDeviceSection(final DynamicTableBuilder table,
+                                  final ConnectionType connection,
+                                  final List<? extends AbstractDevice> devices) {
+        table.span(String.format(LogMessage.UI_CONFIG_TABLE_CONNECTION_TYPE_ROW,
+                computeConnectionType(connection)));
+        table.row(LogMessage.UI_CONFIG_TABLE_PLATFORM_HEADER,
                 LogMessage.UI_CONFIG_TABLE_DEVICE_ALIAS_HEADER,
-                LogMessage.UI_CONFIG_TABLE_CONNECTION_TYPE_HEADER,
-                LogMessage.UI_CONFIG_TABLE_ENABLED_HEADER
-        );
+                LogMessage.UI_CONFIG_TABLE_ENABLED_HEADER);
+        for (AbstractDevice device : devices) {
+            table.row(computeRowColor(device.isEnabled()),
+                    device.getPlatformName().value(),
+                    device.getAlias(),
+                    device.isEnabled());
+        }
     }
 
-    private void addMobileDataRow(final DynamicTableBuilder table,
-                                  final Mobilebrowser mobilebrowser,
-                                  final String aliases) {
-        table.row(
-                computeRowColor(mobilebrowser.isEnabled()),
-                mobilebrowser.getBaseUrl(),
-                aliases,
-                computeConnectionType(mobilebrowser.getConnection()),
-                mobilebrowser.isEnabled()
-        );
-    }
-
-    private void addNativeHeaders(final DynamicTableBuilder table) {
-        table.row(
-                LogMessage.UI_CONFIG_TABLE_NATIVE_PLATFORM_HEADER,
-                LogMessage.UI_CONFIG_TABLE_DEVICE_ALIAS_HEADER,
-                LogMessage.UI_CONFIG_TABLE_CONNECTION_TYPE_HEADER,
-                LogMessage.UI_CONFIG_TABLE_ENABLED_HEADER
-        );
-    }
-
-    private void addNativeDataRow(final DynamicTableBuilder table,
-                                  final Native nativeConfiguration,
-                                  final String platforms,
-                                  final String aliases) {
-        table.row(
-                computeRowColor(nativeConfiguration.isEnabled()),
-                platforms,
-                aliases,
-                computeConnectionType(nativeConfiguration.getConnection()),
-                nativeConfiguration.isEnabled()
-        );
-    }
-
-    private void addSectionTitle(final DynamicTableBuilder table, final String title) {
-        table.span(title);
-    }
-
-    private <T> String joinValues(final List<T> items, final Function<? super T, String> getter) {
-        return items.stream()
-                .map(getter)
-                .collect(Collectors.joining(LogMessage.EMPTY_DELIMITER));
+    private void addSectionHeader(final DynamicTableBuilder table,
+                                  final String titleFormat,
+                                  final boolean isEnabled) {
+        String enabledDetail = isEnabled
+                ? LogMessage.UI_CONFIG_TABLE_ENABLED_DETAIL
+                : LogMessage.UI_CONFIG_TABLE_DISABLED_DETAIL;
+        table.span(computeRowColor(isEnabled), String.format(titleFormat, enabledDetail));
     }
 
     private String computeConnectionType(final ConnectionType connectionType) {
